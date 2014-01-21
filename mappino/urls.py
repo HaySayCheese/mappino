d065c0dcb762a4e82a36e1359b55a9d2dd260a59
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-01-21 23:26:46 +0200

    url для відновленян паролю

--- mappino/urls.py
+++ mappino/urls.py
@@ -27,6 +27,7 @@ urlpatterns += patterns('apps',
     # login and registration
 	url(r'^ajax/api/accounts/registration/$', 'accounts.accounts_ajax.registration_handler'),
 	url(r'^ajax/api/accounts/login/$', 'accounts.accounts_ajax.login_handler'),
+	url(r'^ajax/api/accounts/password-reset/$', 'accounts.accounts_ajax.password_reset_handler'),
 
         # validators
 	    url(r'^ajax/api/accounts/validate-email/$', 'accounts.accounts_ajax.validate_email_handler'),
