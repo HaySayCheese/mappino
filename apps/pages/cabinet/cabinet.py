a3d11fdc36efd755885a56cd3bf6bdf1b0cae6d8
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-01-24 00:29:51 +0200

    fix csrf cookie на вході в кабінет

--- apps/pages/cabinet/cabinet.py
+++ apps/pages/cabinet/cabinet.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 from mappino.wsgi import templates
 
 
+@ensure_csrf_cookie
 def cabinet(request):
 	template = templates.get_template('cabinet/cabinet.html')
 	return HttpResponse(content=template.render())
