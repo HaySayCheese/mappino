764944fd559d92344b30481be0084df9165ed41d
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-01-24 00:58:18 +0200

    fix id кольорів

--- core/dirtags/constants.py
+++ core/dirtags/constants.py
@@ -6,13 +6,13 @@ class DirtagsColors(AbstractConstant):
 	def __init__(self):
 		super(DirtagsColors, self).__init__()
 		self.set_ids({
-			'0': '',
-			'1': '',
-			'2': '',
-			'3': '',
-			'4': '',
-			'5': '',
-			'6': '',
-			'7': '',
+			'0': '0',
+			'1': '1',
+			'2': '2',
+			'3': '3',
+			'4': '4',
+			'5': '5',
+			'6': '6',
+			'7': '7',
 		})
 DIR_TAGS_COLORS = DirtagsColors()
