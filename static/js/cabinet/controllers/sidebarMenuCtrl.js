86c711be66e8528da82b63d47691b8a00accc661
Sergei Meketyuk <SergeiMeketyuk@gmail.com>
2014-01-23 20:32:20 +0200

    Діалог створення тега

--- static/js/cabinet/controllers/sidebarMenuCtrl.js
+++ static/js/cabinet/controllers/sidebarMenuCtrl.js
@@ -1,5 +1,30 @@
 'use strict';
 
-app.controller('SidebarMenuCtrl', function($scope, $rootScope) {
+app.controller('SidebarMenuCtrl', function($scope, $rootScope, $compile) {
+
+    /**
+     * Змінні створення тега
+     **/
+    $scope.newTag = {
+        colors:         ["#33CCFF", "#33FFCC", "#FF6633", "#FF3366", "#3366FF", "#FF33CC", "#FFCC33", "#66FF33"],
+
+        defaultTagName: "Название",
+        tagName:        "Название",
+
+        defaultColor:   "#33CCFF",
+        selectedColor:  "#33CCFF"
+    };
+
+
+    /**
+     * Повернення змінних на базові значеня
+     * після закриття діалога створення тега
+     **/
+    $scope.closeCreateTagDialog = function() {
+        $scope.newTag.selectedColor = $scope.newTag.defaultColor;
+        $scope.newTag.tagName       = $scope.newTag.defaultTagName;
+
+        $scope.creatingTag = false;
+    }
 
 });
