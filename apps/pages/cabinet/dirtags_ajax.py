83c38dbd16692e62b27b3aec3fda2da80ecaae10
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-01-27 01:25:27 +0200

    fix: dirtags API

--- apps/pages/cabinet/dirtags_ajax.py
+++ apps/pages/cabinet/dirtags_ajax.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.http import require_http_methods
 from collective.decorators.views import login_required_or_forbidden
 from collective.exceptions import RecordAlreadyExists
-from collective.methods.request_data_getters import angular_post_parameters
+from collective.methods.request_data_getters import angular_post_parameters, angular_put_parameters
 from core.dirtags.constants import DIR_TAGS_COLORS
 from core.dirtags.models import DirTags
 
@@ -20,11 +20,11 @@ DT_GET_RESPONSES = {
 }
 DT_POST_RESPONSES = {
     'invalid_params': {
-		'code': '1',
+		'code': 1,
         'message': None
     },
     'duplicated_title': {
-		'code': '1',
+		'code': 1,
         'message': '@title is duplicated.'
     },
     'OK': {
@@ -35,15 +35,15 @@ DT_POST_RESPONSES = {
 }
 DT_PUT_RESPONSES = {
     'invalid_params': {
-		'code': '1',
+		'code': 1,
         'message': None
     },
     'invalid_id': {
-		'code': '2',
+		'code': 2,
         'message': 'invalid dirtag id.'
     },
     'invalid_color_id': {
-		'code': '3',
+		'code': 3,
         'message': 'invalid dirtag color_id.'
     },
     'OK': {
@@ -53,7 +53,7 @@ DT_PUT_RESPONSES = {
 }
 DT_DELETE_RESPONSES = {
     'invalid_id': {
-		'code': '2',
+		'code': 2,
         'message': 'invalid dirtag id.'
     },
     'OK': {
@@ -78,14 +78,14 @@ def dirtags_handler(request, dirtag_id=None):
 
 	elif request.method == 'POST':
 		try:
-			d = angular_post_parameters(request, ['title', 'color'])
+			d = angular_post_parameters(request, ['title', 'color_id'])
 		except ValueError as e:
 			response = copy.deepcopy(DT_POST_RESPONSES['invalid_params']) # Note: deepcopy here
 			response['message'] = e.message
 			return HttpResponseBadRequest(json.dumps(response), content_type='application/json')
 
 		try:
-			dirtag = DirTags.new(request.user.id, d['title'], d['color'])
+			dirtag = DirTags.new(request.user.id, d['title'], d['color_id'])
 		except RecordAlreadyExists:
 			return HttpResponse(json.dumps(
 				DT_POST_RESPONSES['duplicated_title']), content_type='application/json')
@@ -96,20 +96,30 @@ def dirtags_handler(request, dirtag_id=None):
 
 
 	elif request.method == 'PUT':
+		if not dirtag_id:
+			HttpResponseBadRequest(
+				json.dumps(DT_PUT_RESPONSES['invalid_id']), content_type='application/json')
+
 		try:
 			dirtag = DirTags.by_id(dirtag_id)
 		except ObjectDoesNotExist:
 			return HttpResponseBadRequest(
 				json.dumps(DT_PUT_RESPONSES['invalid_id']), content_type='application/json')
 
-		d = angular_post_parameters(request)
+		try:
+			d = angular_put_parameters(request, ['title', 'color_id'])
+		except ValueError as e:
+			response = copy.deepcopy(DT_PUT_RESPONSES['invalid_params']) # Note: deepcopy here
+			response['message'] = e.message
+			return HttpResponseBadRequest(json.dumps(response), content_type='application/json')
+
 		title = d.get('title', '')
 		if title:
 			dirtag.title = title
 
-		color_id = d.get('color', '')
-		if color_id:
-			if color_id not in DIR_TAGS_COLORS.values():
+		color_id = d.get('color_id', '')
+		if color_id != '':
+			if color_id not in DIR_TAGS_COLORS.keys():
 				return HttpResponseBadRequest(
 					json.dumps(DT_PUT_RESPONSES['invalid_color_id']), content_type='application/json')
 			dirtag.color_id = color_id
