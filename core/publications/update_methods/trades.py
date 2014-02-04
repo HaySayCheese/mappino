11fba2653930a638d1539109dc0c7b02ef1447f6
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-02-04 15:46:36 +0200

    RC3 обробників на зміну даних в БД

--- core/publications/update_methods/trades.py
+++ core/publications/update_methods/trades.py
@@ -681,7 +681,7 @@ def update_trade(h, field, value):
 
 
 		# boolean
-		elif field == 'cab_tv':
+		elif field == 'cable_tv':
 			if (value is True) or (value is False):
 				b = TradesBodies.objects.filter(id=h.body_id).only('id')[0]
 				b.cable_tv = value
