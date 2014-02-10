be49fb82c81be005b7d00d4959bee69a25a7282b
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-02-10 22:22:57 +0200

    fix: заборона входу в адмінку неавторизованих користувачів

--- apps/pages/cabinet/cabinet.py
+++ apps/pages/cabinet/cabinet.py
@@ -1,9 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import ensure_csrf_cookie
 from mappino.wsgi import templates
 
 
 @ensure_csrf_cookie
 def cabinet(request):
+	if not request.user.is_authenticated():
+		return HttpResponseRedirect('/#!/account/login')
+
 	template = templates.get_template('cabinet/cabinet.html')
 	return HttpResponse(content=template.render())
