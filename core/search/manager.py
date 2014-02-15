9a5a697e2ab25e35590b2ed5abd521de7cc9eef0
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-02-15 18:05:57 +0200

    fix

--- core/search/manager.py
+++ core/search/manager.py
@@ -1,6 +1,9 @@
 #coding=utf-8
+import Queue
+
 import MySQLdb
 from django.conf import settings
+
 from core.publications.constants import OBJECTS_TYPES
 from core.publications.models_signals import updated as publication_model_updated
 from core.search.tasks import update_house_index
@@ -13,17 +16,45 @@ class SearchManager(object):
 	def __init__(self):
 		self.redis = redis_connections['cache']
 		self.prefix = 'search_idx_task_'
-		# self.update_interval = 60 * 3 # secs
-		self.update_interval = 1 # secs
+		self.update_interval = 60 * 3 # secs
 
-		self.cursor = None
-		self.__reconnect_to_sphinx()
+		self.connections = Queue.Queue()
+		for i in xrange(4):
+			self.connections.put(self.new_connection())
 
-		publication_model_updated.connect(self.model_updated)
+		publication_model_updated.connect(self.update_models_index)
 
 
+	@staticmethod
+	def new_connection():
+		return MySQLdb.connect(
+			host = settings.SPHINX_SEARCH['HOST'],
+			port = settings.SPHINX_SEARCH['PORT'],
+		)
 
-	def model_updated(self, **kwargs):
+
+	def __execute_query(self, query):
+		connection = self.connections.get(block=True)
+
+		def query_results():
+			cursor = connection.cursor()
+			cursor.execute(query)
+			return cursor.fetchall()
+
+		try:
+			results = query_results()
+		except MySQLdb.OperationalError:
+			# Якщо втрачено з’єднання - спробувати повторно з’єднатись.
+			# OperationalError може свідчити і про інші помилки,
+			# тож не варто перехоплювати дану викл. ситуацію більше одного разу.
+			connection = self.new_connection()
+			results = query_results()
+
+		self.connections.put(connection)
+		return results
+
+
+	def update_models_index(self, **kwargs):
 		try:
 			tid = kwargs['tid']
 			hid = kwargs['hid']
@@ -46,50 +77,37 @@ class SearchManager(object):
 			pipe = self.redis.pipeline()
 			pipe.set(key, task.id)
 			pipe.expire(key, self.update_interval)
-			pipe.execute()
+			pipe.__execute_query()
 		except Exception:
 			# Всі виключні ситуації подавляються умисно.
-			# Таким чином, навіть якщо celery чи sphinx відпадуть - це ніяк не вплине на роботу інших,
-			# пов’язаних із цією підсистем і запит принаймні виконається.
+			# Таким чином, навіть якщо celery чи sphinx відпадуть — це ніяк не вплине на роботу інших,
+			# пов’язаних із цією підсистем, і запит принаймні виконається.
 			# Про виключні ситуації в даному модулі слід повідомляти в лог або адмінам на email.
 			# todo: add log record here or email
 			pass
 
 
 	def task_digest(self, tid, hid):
+		"""
+		Повертає унікальний id для запису про те,
+		що запит на оновлення індексу вже присутній в черзі
+		"""
 		return self.prefix + unicode(tid) + ':' +  unicode(hid)
 
 
 	def process_search_query(self, query, user_id):
-		def execute():
-			self.cursor.execute(u"SELECT tid, hid FROM publications_rt "
-			                "WHERE MATCH('{query}') AND uid = {uid} LIMIT 50".format(
-				query = query, uid = user_id))
+		query = u"SELECT tid, hid FROM publications_rt " \
+		        u"WHERE MATCH('{query}') AND uid = {uid} LIMIT 50".format(
+				query = query, uid = user_id)
 
-			results = {}
-			for record in self.cursor.fetchall():
-				tid = record[0]
-				hid = record[1]
+		results = {}
+		for record in self.__execute_query(query):
+			tid = record[0]
+			hid = record[1]
 
-				if tid in results:
-					results[tid].append(hid)
-				else:
-					results[tid] = [hid]
-			return results
+			if tid in results:
+				results[tid].append(hid)
+			else:
+				results[tid] = [hid]
+		return results
 
-		try:
-			return execute()
-		except MySQLdb.OperationalError:
-			# Якщо втрачено з’єднання - спробувати повторно з’єднатись.
-			# OperationalError може свідчити і про інші помилки,
-			# тож не варто перехоплювати дану викл. ситуацію більше одного разу.
-			self.__reconnect_to_sphinx()
-			return execute()
-
-
-
-	def __reconnect_to_sphinx(self):
-		self.cursor = MySQLdb.connect(
-			host = settings.SPHINX_SEARCH['HOST'],
-			port = settings.SPHINX_SEARCH['PORT'],
-		).cursor()
