7791d2788ad54c096b4bbfc9b1ac1b0c7d7038a4
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-02-16 19:46:36 +0200

    fix швидкодії вибірки брифів

--- apps/pages/cabinet/briefs/utils.py
+++ apps/pages/cabinet/briefs/utils.py
@@ -8,13 +8,10 @@ from core.publications.constants import HEAD_MODELS, OBJECTS_TYPES, OBJECT_STATE
 
 def briefs_of_tag(tag):
 	ids = tag.publications_hids()
-	if not ids:
-		return []
 
 	pubs = []
 	for tid in ids.keys():
-		query = HEAD_MODELS[tid].objects.filter(id__in = ids[tid]).only(
-			'id', 'for_sale', 'for_rent', 'body__title')
+		query = HEAD_MODELS[tid].objects.filter(id__in = ids[tid]).only('id')
 		pubs.extend(__dump_publications_list(tid, tag.user.id, query))
 	return pubs
 
@@ -23,7 +20,7 @@ def briefs_of_tag(tag):
 def briefs_of_section(section, user_id):
 	pubs = []
 	for tid in OBJECTS_TYPES.values():
-		query = HEAD_MODELS[tid].by_user_id(user_id).only('id', 'for_sale', 'for_rent', 'body__title')
+		query = HEAD_MODELS[tid].by_user_id(user_id).only('id')
 
 		if section == 'published':
 			query = query.filter(state_sid = OBJECT_STATES.published())
@@ -43,35 +40,32 @@ def briefs_of_publications(ids, user_id):
 
 	pubs = []
 	for tid in ids.keys():
-		query = HEAD_MODELS[tid].by_user_id(user_id).filter(id__in = ids[tid]).only(
-			'id', 'for_sale', 'for_rent', 'body__title')
+		query = HEAD_MODELS[tid].by_user_id(user_id).filter(id__in = ids[tid]).only('id')
 		pubs.extend(__dump_publications_list(tid, user_id, query))
 	return pubs
 
 
 
 def __dump_publications_list(tid, user_id, queryset):
-	pub_ids = [publication.id for publication in queryset]
-	tags = DirTags.contains_publications(tid, pub_ids).filter(user_id = user_id).only('id', 'pubs')
-
-	if queryset:
-		return [{
-			'tid': tid,
-			'id': publication.id,
-		    'state_sid': publication.state_sid,
-		    'title': publication.body.title,
-		    'for_sale': publication.for_sale,
-		    'for_rent': publication.for_rent,
-		    'tags': [tag.id for tag in ifilter(lambda t: t.contains(tid, publication.id), tags)],
-		    'photo_url': 'http://localhost/mappino_static/img/cabinet/house.png' # fixme
-
-		    # ...
-		    # other fields here
-		    # ...
-
-		} for publication in queryset]
-	else:
+	publications_list = queryset.values_list('id', 'state_sid', 'body__title', 'for_rent', 'for_sale')
+	if not publications_list:
 		return []
 
+	pub_ids = [publication[0] for publication in publications_list] # publication[0] = publication.id
+	tags = DirTags.contains_publications(tid, pub_ids).filter(user_id = user_id).only('id', 'pubs')
 
+	return [{
+		'tid': tid,
+		'id': publication[0], # id
+	    'state_sid': publication[1], # state_sid
+	    'title': publication[2], # body.title
+	    'for_sale': publication[3], # for_sale
+	    'for_rent': publication[4], # for_rent
+	    'tags': [tag.id for tag in ifilter(lambda t: t.contains(tid, publication[0]), tags)],
+	    'photo_url': 'http://localhost/mappino_static/img/cabinet/house.png' # fixme
 
+	    # ...
+	    # other fields here
+	    # ...
+
+	} for publication in publications_list]
