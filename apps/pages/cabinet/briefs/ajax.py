d0c0f35997000163c1a82660005d5fc8674c7ee3
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-02-24 14:37:11 +0200

    fix

--- apps/pages/cabinet/briefs/ajax.py
+++ apps/pages/cabinet/briefs/ajax.py
@@ -2,26 +2,34 @@
 import json
 
 from django.core.exceptions import PermissionDenied
-from django.db import connection
 from django.http import HttpResponseBadRequest, HttpResponse
 from django.views.decorators.http import require_http_methods
 
 from apps.pages.cabinet.briefs.utils import briefs_of_tag, briefs_of_section, get_sections_counters
-
 from collective.decorators.views import login_required_or_forbidden
 from core.dirtags import DirTags
-from core.publications.constants import HEAD_MODELS, OBJECTS_TYPES, OBJECT_STATES
+
 
 
 get_codes = {
 	'invalid_tag_id': {
 		'code': 1,
 	},
+    'invalid_section': {
+		'code': 2,
+	},
 }
 @login_required_or_forbidden
 @require_http_methods('GET')
 def get(request, tag_id=None, section=None):
-	if tag_id is not None:
+	# section за замовчуванням None для того, щоб розмістити даний параметр за tag_id,
+	# інакше, довелось би писати ще одну функція на віддачу брифів для тегів окремо.
+	if section is None:
+		return HttpResponseBadRequest(
+			json.dumps(get_codes['invalid_section']), content_type='application/json')
+
+
+	if (section == 'tag') and (tag_id is not None):
 		try:
 			tag = DirTags.objects.filter(id = tag_id).only('id', 'pubs', 'user')[0]
 		except IndexError:
@@ -31,20 +39,18 @@ def get(request, tag_id=None, section=None):
 		# check owner
 		if tag.user_id != request.user.id:
 			raise PermissionDenied()
-		return HttpResponse(json.dumps(briefs_of_tag(tag)), content_type='application/json')
+		return HttpResponse(
+			json.dumps(briefs_of_tag(tag)), content_type='application/json')
+			# todo: додати код 0 (ok) у відповідь
 
 
 	else:
-		return HttpResponse(json.dumps(
-			briefs_of_section(section, request.user.id)), content_type='application/json')
+		return HttpResponse(
+			json.dumps(briefs_of_section(section, request.user.id)), content_type='application/json')
+			# todo: додати код 0 (ok) у відповідь
 
 
 
-# get_counters_codes = {
-# 	'invalid_tag_id': {
-# 		'code': 1,
-# 	},
-# }
 @login_required_or_forbidden
 @require_http_methods('GET')
 def get_counters(request):
--- apps/pages/cabinet/briefs/utils.py
+++ apps/pages/cabinet/briefs/utils.py
@@ -23,12 +23,16 @@ def briefs_of_section(section, user_id):
 	for tid in OBJECTS_TYPES.values():
 		query = HEAD_MODELS[tid].by_user_id(user_id).only('id')
 
-		if section == 'published':
+		if section == 'all':
+			pass
+		elif section == 'published':
 			query = query.filter(state_sid = OBJECT_STATES.published())
 		elif section == 'unpublished':
 			query = query.filter(state_sid = OBJECT_STATES.unpublished())
 		elif section == 'deleted':
 			query = query.filter(state_sid = OBJECT_STATES.deleted())
+		else:
+			raise ValueError('Invalid section title {0}'.format(section))
 
 		pubs.extend(__dump_publications_list(tid, user_id, query))
 	return pubs
@@ -36,6 +40,15 @@ def briefs_of_section(section, user_id):
 
 
 def briefs_of_publications(ids, user_id):
+	"""
+	Args:
+		ids: словник формату tid: [hid1, hid2, ... hidN].
+		user_id: id користувача, якому належать оголошення з ids.
+
+
+	Повертає брифи оголошень з id зі списку ids та таких, що належать користувачу user_id.
+	Використовується, наприклад, для того, щоб видати брифи оголошень знайдених пошуком.
+	"""
 	if not ids:
 		return []
 
@@ -49,8 +62,10 @@ def briefs_of_publications(ids, user_id):
 
 def get_sections_counters(user_id):
 	"""
-	Поверне словник розділів і тегів користувача з id = user_id.
+	Поверне список розділів і тегів коритсувача з id = user_id.
 	Кожен запис супроводжуватиметься кількістю оголошень в кожному конкретному розділі чи тезі.
+
+	SQL-запит використовується тому, що на orm логіка вибірки була б вкрай важкою, а к-сть запитів значно зросла б.
 	"""
 
 	# tags
@@ -195,8 +210,9 @@ def get_sections_counters(user_id):
 
 	published = int(count[0][0])
 	unpublished = int(count[1][0])
-	deleted = int(count[2][0])
 	all = published + unpublished
+
+	deleted = int(count[2][0])
 
 	counters.update({
 		'all': all,
@@ -208,6 +224,14 @@ def get_sections_counters(user_id):
 
 
 def __dump_publications_list(tid, user_id, queryset):
+	"""
+	Повератає список брифів оголошень, вибраних у queryset.
+
+	Note:
+		queryset передається, а не формуєтсья в даній функції для того,
+		щоб на вищих рівнях можна було накласти додакові умови на вибірку.
+		По суті, дана функція лишш дампить результати цієї вибірки в список в певному форматі.
+	"""
 	publications_list = queryset.values_list('id', 'state_sid', 'body__title', 'for_rent', 'for_sale')
 	if not publications_list:
 		return []
