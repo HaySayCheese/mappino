415e900032b736781407e1de30e3daadcc4316c0
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-02-26 08:38:53 +0200

    urls refactoring

--- mappino/urls.py
+++ mappino/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls import patterns, url
 
 #-- angular templates for main pages
 urlpatterns = patterns('apps.pages.main',
+    #-- common
     url(r'^ajax/template/main/first-enter/$', 'templates_ajax.first_enter_template'),
     url(r'^ajax/template/main/search/$', 'templates_ajax.search_template'),
     url(r'^ajax/template/main/detailed/$', 'templates_ajax.search_template'),
 
-    # filters
+
+    #-- filters forms
     url(r'^ajax/template/main/filters/(\d+)/$', 'templates_ajax.filter_form_template'),
 )
 
@@ -20,25 +22,23 @@ urlpatterns += patterns('apps.accounts',
 
 #-- angular templates for cabinet pages
 urlpatterns += patterns('apps.pages.cabinet',
+    #-- publications
     url(r'^ajax/template/cabinet/publications/$', 'templates_ajax.publications_template'),
-)
 
-#-- angular templates publications
-urlpatterns += patterns('apps.pages.cabinet',
-	# unpublished
-    url(r'^ajax/template/cabinet/publications/(\d+)/$', 'templates_ajax.publication_form_template'),
-    url(r'^ajax/template/cabinet/publications/map/$', 'templates_ajax.map_template'),
-    url(r'^ajax/template/cabinet/publications/photos/$', 'templates_ajax.photos_template'),
+	    # unpublished publications forms
+	    url(r'^ajax/template/cabinet/publications/(\d+)/$', 'templates_ajax.publication_form_template'),
+	    url(r'^ajax/template/cabinet/publications/map/$', 'templates_ajax.map_template'),
+	    url(r'^ajax/template/cabinet/publications/photos/$', 'templates_ajax.photos_template'),
 
-	# published
-    url(r'^ajax/template/cabinet/published/(\d+)/$', 'templates_ajax.published_publication_form_template'),
+		# published publications forms
+	    url(r'^ajax/template/cabinet/published/(\d+)/$', 'templates_ajax.published_publication_form_template'),
 )
 
 
 
 #-- angular API for main pages
 urlpatterns += patterns('apps',
-    # login and registration
+    #-- login and registration
 	url(r'^ajax/api/accounts/registration/$', 'accounts.accounts_ajax.registration_handler'),
     url(r'^ajax/api/accounts/registration/cancel/$', 'accounts.accounts_ajax.registration_cancel_handler'),
     url(r'^ajax/api/accounts/registration/resend-sms/$', 'accounts.accounts_ajax.resend_sms_handler'),
@@ -55,53 +55,55 @@ urlpatterns += patterns('apps',
         url(r'^ajax/api/accounts/on-login-info/$', 'accounts.accounts_ajax.on_login_info_handler'),
 
 
-    # markers
+    #-- markers
     url(r'^ajax/api/markers/$', 'pages.main.markers.ajax.get_markers'),
 )
 
 #-- angular API for cabinet
 urlpatterns += patterns('apps.pages.cabinet',
-    # dirtags
+    #-- dirtags
 	url(r'^ajax/api/cabinet/dirtags/$', 'dirtags.ajax.dirtags_handler'),
     url(r'^ajax/api/cabinet/dirtags/(\d+)/$', 'dirtags.ajax.dirtags_handler'),
     url(r'^ajax/api/cabinet/dirtags/(\d+)/add-publication/(\d+:\d+)$', 'dirtags.ajax.dirtags_handler'),
 
 
-    # publications
-    url(r'^ajax/api/cabinet/publications/counters/$', 'briefs.ajax.get_counters'),
-    url(r'^ajax/api/cabinet/publications/briefs/all/$',
-        'briefs.ajax.get', {'section': 'all'}),
-    url(r'^ajax/api/cabinet/publications/briefs/published/$',
-        'briefs.ajax.get', {'section': 'published'}),
-    url(r'^ajax/api/cabinet/publications/briefs/unpublished/$',
-        'briefs.ajax.get', {'section': 'unpublished'}),
-    url(r'^ajax/api/cabinet/publications/briefs/deleted/$',
-        'briefs.ajax.get', {'section': 'deleted'}),
-    url(r'^ajax/api/cabinet/publications/briefs/(\d+)/$',
-        'briefs.ajax.get', {'section': 'tag'}),
+    #-- publications
+	    # briefs
+	    url(r'^ajax/api/cabinet/publications/counters/$', 'briefs.ajax.get_counters'),
+	    url(r'^ajax/api/cabinet/publications/briefs/all/$',
+	        'briefs.ajax.get', {'section': 'all'}),
+	    url(r'^ajax/api/cabinet/publications/briefs/published/$',
+	        'briefs.ajax.get', {'section': 'published'}),
+	    url(r'^ajax/api/cabinet/publications/briefs/unpublished/$',
+	        'briefs.ajax.get', {'section': 'unpublished'}),
+	    url(r'^ajax/api/cabinet/publications/briefs/deleted/$',
+	        'briefs.ajax.get', {'section': 'deleted'}),
+	    url(r'^ajax/api/cabinet/publications/briefs/(\d+)/$',
+	        'briefs.ajax.get', {'section': 'tag'}),
 
-	url(r'^ajax/api/cabinet/publications/$', 'publications.ajax.create'),
-	url(r'^ajax/api/cabinet/publications/(\d+:\d+)/$', 'publications.ajax.rud_switch'),
-    url(r'^ajax/api/cabinet/publications/(\d+:\d+)/publish/$', 'publications.ajax.publish'),
-    url(r'^ajax/api/cabinet/publications/(\d+:\d+)/unpublish/$', 'publications.ajax.unpublish'),
+		# CRUD
+		url(r'^ajax/api/cabinet/publications/$', 'publications.ajax.create'),
+		url(r'^ajax/api/cabinet/publications/(\d+:\d+)/$', 'publications.ajax.rud_switch'),
+	    url(r'^ajax/api/cabinet/publications/(\d+:\d+)/publish/$', 'publications.ajax.publish'),
+	    url(r'^ajax/api/cabinet/publications/(\d+:\d+)/unpublish/$', 'publications.ajax.unpublish'),
 
-    # photos
-    url(r'^ajax/api/cabinet/publications/(\d+:\d+)/photos/$', 'publications.photos_ajax.upload'),
-    url(r'^ajax/api/cabinet/publications/(\d+:\d+)/photos/(\d+)/$', 'publications.photos_ajax.rud_switch'),
+	    # photos
+	    url(r'^ajax/api/cabinet/publications/(\d+:\d+)/photos/$', 'publications.photos_ajax.upload'),
+	    url(r'^ajax/api/cabinet/publications/(\d+:\d+)/photos/(\d+)/$', 'publications.photos_ajax.rud_switch'),
 
 
-    # search
+    #-- search
 	url(r'^ajax/api/cabinet/search/$', 'search.ajax.search'),
 )
 
 
 
-#-- main pages
+#-- main pages html
 urlpatterns += patterns('apps.pages.main',
     url(r'^$', 'home.home'),
 )
 
-#-- cabinet pages
+#-- cabinet pages html
 urlpatterns += patterns('apps.pages.cabinet',
     url(r'^cabinet/$', 'cabinet.cabinet'),
 )
