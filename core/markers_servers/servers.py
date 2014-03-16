4d34bd41ff02b3be4122046a5363dec5cebbee22
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-03-16 19:47:04 +0200

    заготовка фільтрів для офісів

--- core/markers_servers/servers.py
+++ core/markers_servers/servers.py
@@ -4415,8 +4415,156 @@ class WarehousesMarkersManager(BaseMarkersManager):
 			pass
 
 
-	def filter(self, publications, conditions):
-		return
+	def filter(self, publications, filters):
+		# WARNING:
+		#   дана функція для економії часу виконання не виконує deepcopy над publications
+
+		if filters is None:
+			return publications
+
+		operation_sid = filters.get('operation_sid')
+		if operation_sid is None:
+			raise ValueError('Invalid conditions. Operation_sid is absent.')
+
+
+		# Перед фільтруванням оголошень слід перевірити цілісність і коректність об’єкту умов.
+		# На даному етапі виконується перевірка всіх обов’язкових полів filters.
+		# Дану перевірку винесено за цикл фільтрування щоб підвищити швидкодію,
+		# оскільки об’єкт filters не змінюється в ході фільтрування і достатньо перевіріити його лише раз.
+		currency_sid = filters.get('currency_sid')
+		if currency_sid is None:
+			# Перевіряти фільтри цін має зміст лише тоді, коли задано валюту фільтру,
+			# інакше неможливо привести валюту ціни з фільтра до валюти з оголошення.
+			# На фронтенді валюта повинна бути задана за замовчуванням.
+			raise ValueError('sale_currency_sid is absent.')
+		elif currency_sid not in CURRENCIES.values():
+			raise ValueError('currency_sid is invalid.')
+
+		if operation_sid not in [0, 1]:
+			raise ValueError('Invalid operation_sid.')
+
+
+		# Для відбору елементів зі списку publications, використовується список statuses.
+		# Кість елементів цього списку відповідає к-сті елементів publications.
+		# На початку фільтрування всі елементи statuses встановлені в True.
+		# Під час фільтрування деякі з них будуть встановлені в False.
+		# На завершальному етапі зі списку publications будуть відібрані лише ті елементи,
+		# відповідний елемент statuses яких встановлений в True.
+		#
+		# Додатковий список використовується для підвищення швидкодії фільтрування,
+		# оскільки зміна True/False відбуваєтсья в рази швидше, ніж вилучення елементів зі списку
+		# з повторною його перебудовою на кожній перевірці та ітерації.
+		statuses = [True] * len(publications)
+
+
+		for i in range(len(statuses)):
+			# Якщо даний запис вже позначений як виключений — не аналізувати його.
+			if not statuses[i]:
+				continue
+
+			marker = publications[i][1]
+
+			# price
+			price_min = filters.get('price_from')
+			price_max = filters.get('price_to')
+			if price_min is not None:
+				price_min = convert_currency(price_min, currency_sid, marker['sale_currency_sid'])
+			if price_max is not None:
+				price_max = convert_currency(price_max, currency_sid, marker['sale_currency_sid'])
+
+
+			if (price_max is not None) and (price_min is not None):
+				if not price_min <= marker['sale_price'] <= price_max:
+					statuses[i] = False
+					continue
+
+			elif price_min is not None:
+				if not price_min <= marker['sale_price']:
+					statuses[i] = False
+					continue
+
+			elif price_max is not None:
+				if not marker['sale_price'] <= price_max:
+					statuses[i] = False
+					continue
+
+
+			# market type
+			if ('new_buildings' in filters) and ('secondary_market' in filters):
+				# Немає змісту фільтрувати.
+				# Під дані умови потрапляють всі об’єкти.
+				pass
+
+			elif 'new_buildings' in filters:
+				statuses[i] = (marker['market_type_sid'] == MARKET_TYPES.new_building())
+			elif 'new_buildings' in filters:
+				statuses[i] = (marker['market_type_sid'] == MARKET_TYPES.secondary_market())
+
+
+			# halls area
+			halls_area_min = filters.get('halls_area_from')
+			halls_area_max = filters.get('halls_area_to')
+			halls_area = marker.get('halls_area')
+
+			if (halls_area_max is not None) or (halls_area_min is not None):
+				# Поле "площа залів" може бути не обов’язковим.
+				# У випадку, коли воно задане у фільтрі, але відсутнє в записі маркера —
+				# відхилити запис через неможливість аналізу.
+				if halls_area is None:
+					statuses[i] = False
+					continue
+
+			if (halls_area_max is not None) and (halls_area_min is not None):
+				if not halls_area_min <= halls_area <= halls_area_max:
+					statuses[i] = False
+					continue
+
+			elif halls_area_min is not None:
+				if not halls_area_min <= halls_area:
+					statuses[i] = False
+					continue
+
+			elif halls_area_max is not None:
+				if not halls_area <= halls_area_max:
+					statuses[i] = False
+					continue
+
+
+			if 'electricity' in filters:
+				if (not 'electricity' in marker) or (not marker['electricity']):
+					statuses[i] = False
+					continue
+
+			if  'gas' in filters:
+				if (not 'gas' in marker) or (not marker['gas']):
+					statuses[i] = False
+					continue
+
+			if 'hot_water' in filters:
+				if (not 'hot_water' in marker) or (not marker['hot_water']):
+					statuses[i] = False
+					continue
+
+			if  'cold_water' in filters:
+				if (not 'cold_water' in marker) or (not marker['cold_water']):
+					statuses[i] = False
+					continue
+
+			if 'security_alarm' in filters:
+				if (not 'security_alarm' in marker) or (not marker['security_alarm']):
+					statuses[i] = False
+					continue
+
+			if 'fire_alarm' in filters:
+				if (not 'fire_alarm' in marker) or (not marker['fire_alarm']):
+					statuses[i] = False
+					continue
+
+		result = []
+		for i in range(len(statuses)):
+			if statuses[i]:
+				result.append(publications[i])
+		return result
 
 
 
