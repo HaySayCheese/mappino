994b705675251ba4da60ef6b7cb28e1825ddb11f
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-04-15 22:35:46 +0300

    fix

--- collective/http/cookies.py
+++ collective/http/cookies.py
@@ -2,35 +2,28 @@ import datetime
 from django.conf import settings
 
 
-DEFAULT_COOKIE_AGE = 365 * 24 * 60 * 60 # one year
+def set_cookie(response, key, value, seconds_expire=None):
+	if seconds_expire is None:
+		seconds_expire = 365 * 24 * 60 * 60 # one year
+
+	expires = datetime.datetime.strftime(
+		datetime.datetime.utcnow() + datetime.timedelta(seconds=seconds_expire), "%a, %d-%b-%Y %H:%M:%S GMT")
+	response.set_cookie(
+		key, value, max_age=seconds_expire, expires=expires,
+		domain=settings.SESSION_COOKIE_DOMAIN,
+		secure=settings.SESSION_COOKIE_SECURE or None
+	)
 
 
-def set_cookie(response, key, value, days_expire=7):
-  if days_expire is None:
-    max_age = DEFAULT_COOKIE_AGE
-  else:
-    max_age = days_expire * 24 * 60 * 60
+def set_signed_cookie(response, key, value, salt='', seconds_expire=None, http_only=True):
+	if seconds_expire is None:
+		seconds_expire = 365 * 24 * 60 * 60 # one year
 
-  expires = datetime.datetime.strftime(
-	  datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
-  response.set_cookie(
-	  key, value, max_age=max_age, expires=expires,
-	  domain=settings.SESSION_COOKIE_DOMAIN,
-	  secure=settings.SESSION_COOKIE_SECURE or None
-  )
-
-
-def set_signed_cookie(response, key, value, salt='', days_expire=7, http_only=True):
-  if days_expire is None:
-    max_age = DEFAULT_COOKIE_AGE
-  else:
-    max_age = days_expire * 24 * 60 * 60
-
-  expires = datetime.datetime.strftime(
-	  datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
-  response.set_signed_cookie(
-	  key, value, salt, max_age=max_age, expires=expires,
-	  domain=settings.SESSION_COOKIE_DOMAIN,
-	  secure=settings.SESSION_COOKIE_SECURE or None,
-      httponly=http_only
-  )
+	expires = datetime.datetime.strftime(
+		datetime.datetime.utcnow() + datetime.timedelta(seconds=seconds_expire), "%a, %d-%b-%Y %H:%M:%S GMT")
+	response.set_signed_cookie(
+		key, value, salt, max_age=seconds_expire, expires=expires,
+		domain=settings.SESSION_COOKIE_DOMAIN,
+		secure=settings.SESSION_COOKIE_SECURE or None,
+		httponly=http_only
+	)
