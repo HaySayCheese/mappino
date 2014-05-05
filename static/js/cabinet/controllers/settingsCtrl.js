c12b17c4f53d08bcbe5b6675ade72fe18a11872f
Sergei Meketyuk <SergeiMeketyuk@gmail.com>
2014-05-05 23:28:41 +0300

    фікси

---
+++ static/js/cabinet/controllers/settingsCtrl.js
@@ -0,0 +1,26 @@
+'use strict';
+
+app.controller('SettingsCtrl', function($scope, $rootScope) {
+
+    initScrollBar();
+
+    /**
+     * Функція скролбара
+     */
+    function initScrollBar() {
+        var sidebar = angular.element(".sidebar-item-detailed-body");
+
+        sidebar.perfectScrollbar("destroy");
+
+        sidebar.perfectScrollbar({
+            wheelSpeed: 40,
+            useKeyboard: false,
+            suppressScrollX: true
+        });
+
+        angular.element(window).resize(function() {
+            sidebar.perfectScrollbar("update");
+        });
+    }
+
+});
