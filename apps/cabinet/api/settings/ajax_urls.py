87bb2a7aa61632a3785fa851d7785bd05300ae85
hsc <HaySayCheese@gmail.com>
2014-05-09 12:32:57 +0300

    fix

--- apps/cabinet/api/settings/ajax_urls.py
+++ apps/cabinet/api/settings/ajax_urls.py
@@ -6,6 +6,7 @@ from apps.cabinet.api.settings.ajax import PersonalPagesAliasesManager
 urlpatterns = patterns('',
     url(r'^ajax/api/cabinet/settings/$', Account.as_view()),
 
-    url(r'^ajax/api/cabinet/settings/personal-page-aliases/validate/$', PersonalPagesAliasesManager.ValidateAlias.as_view()),
-    url(r'^ajax/api/cabinet/settings/personal-page-aliases/$', PersonalPagesAliasesManager.SetAlias.as_view()),
+	# todo: enable me back
+    # url(r'^ajax/api/cabinet/settings/personal-page-aliases/validate/$', PersonalPagesAliasesManager.ValidateAlias.as_view()),
+    # url(r'^ajax/api/cabinet/settings/personal-page-aliases/$', PersonalPagesAliasesManager.SetAlias.as_view()),
 )
