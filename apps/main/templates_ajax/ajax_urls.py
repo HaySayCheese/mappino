f2cb3b4bd0f40effeb2d848f6942e71f3630c299
hsc <HaySayCheese@gmail.com>
2014-06-07 12:15:01 +0300

    templates for the common sidebar and the realtor's page

--- apps/main/templates_ajax/ajax_urls.py
+++ apps/main/templates_ajax/ajax_urls.py
@@ -8,9 +8,16 @@ urlpatterns = patterns('apps.main.templates_ajax',
 	url(r'^ajax/template/main/accounts/access-restore/$', 'accounts.access_restore_template'),
 
 
+	# sidebar templates
+    url(r'^ajax/template/main/sidebar/common/$', 'sidebars.common_sidebar_template'),
+    url(r'^ajax/template/main/sidebar/realtors/$', 'sidebars.realtors_sidebar_template'),
+
+
     # publications search engine templates
-    url(r'^ajax/template/main/first-enter/$', 'publications_search.first_enter_template'),
-    url(r'^ajax/template/main/search/$', 'publications_search.search_template'),
     url(r'^ajax/template/main/filters/(\w+)/(\d+)/$', 'publications_search.filters_form_template'),
     url(r'^ajax/template/main/detailed-dialog/$', 'publications_search.detailed_dlg_template'),
+
+		# todo: remove them if not needed
+	    # url(r'^ajax/template/main/first-enter/$', 'publications_search.first_enter_template'),
+	    # url(r'^ajax/template/main/search/$', 'publications_search.search_template'),
 )
