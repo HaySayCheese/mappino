71005ab1e562de0b3197f94ce3adf61cbf60bc93
Sergei <SergeiMeketyuk@gmail.com>
2014-06-18 15:26:02 +0300

    fix

--- static/js/promo/promo.js
+++ static/js/promo/promo.js
@@ -6,8 +6,7 @@ $(document).ready(function() {
         headerLinksBlock = scrollableBlock.find(".header-links");
 
     scrollableBlockImage.imageScroll({
-        container: $('.wrapper'),
-        mediaHeight: 1080
+        container: $('.wrapper')
     });
 
 
@@ -15,10 +14,16 @@ $(document).ready(function() {
         var scrollTop = scrollableBlock.scrollTop();
 
         $(".image-caption")
-            .css("opacity", (100 / (scrollTop / 6)) - 1)
             .css("top", '50%')
-            .css("top", '+=' + (scrollTop / 1.5));
+            .css("top", '+=' + (scrollTop / 1.5))
+            .find("h1")
+            .css("opacity", (100 / (scrollTop / 6)) - 2)
+            .parent()
+            .find("img")
+            .css("opacity", (100 / (scrollTop / 6)) - 1);
 
+
+        /** Navbar */
         if (scrollTop > $(window).height() - 70) {
             headerLinksBlock.removeClass("slideOutUp").addClass("slideInDown panel");
         } else if (headerLinksBlock.hasClass("panel") && !headerLinksBlock.hasClass("slideOutUp")) {
@@ -36,6 +41,32 @@ $(document).ready(function() {
 
                         next();
                     });
+        }
+
+        /** Markers */
+        var sections = $("section"),
+            markers = $(".tablet-marker");
+
+        if (scrollTop > sections[0].offsetTop - 100) {
+
+            $.each(markers, function(i, el) {
+//                $(el).addClass("fadeInDown");
+
+                setTimeout(function() {
+                    $(el).addClass("fadeInDown");
+                }, 300 + (i * 300));
+            });
+
+
+//            for (var i = 0; i <= markers.length; i++) {
+//                $(".tablet-marker.m-" + i)
+//                    //.addClass("fadeInDown")
+//                    .delay(250).queue(function(next) {
+//                        $(this).addClass("fadeInDown");
+//
+//                        next();
+//                    });
+//            }
         }
     });
 
