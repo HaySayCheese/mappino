5fa814feabc383c8b3235cb12f7949d7c108fb5f
Sergei <SergeiMeketyuk@gmail.com>
2014-06-20 11:53:42 +0300

    fix

--- static/js/promo/promo.js
+++ static/js/promo/promo.js
@@ -45,9 +45,6 @@ $(document).ready(function() {
 
                         next();
                     });
-
-            $("a").removeClass("active");
-            window.location.hash = "main";
         }
 
         /** Markers */
@@ -55,7 +52,6 @@ $(document).ready(function() {
             markers = $(".tablet-marker");
 
         if (scrollTop > sections[0].offsetTop - 150) {
-
             $.each(markers, function(i, el) {
                 setTimeout(function() {
                     $(el).addClass("fadeInDown");
@@ -63,32 +59,5 @@ $(document).ready(function() {
             });
         }
     });
-
-
-    /** Скролимо до секції якщо є хеш в урлі */
-    window.location.hash && animateScroll(window.location.hash);
-
-
-    /** Скролимо до секції по кліку на ссилки */
-    headerLinksBlock.find('a').click(function(e) {
-        animateScroll($(this).attr("href"));
-
-        e.preventDefault();
-    });
-
-
-
-    /** Функція яка забезпечує плавний скрол */
-    function animateScroll(href) {
-        $('html, body').animate({
-            scrollTop: href != "#main" ? $(href).offset().top - 69 : 0
-        }, 500, function () {
-            window.location.hash = href;
-        });
-
-
-        $("a").removeClass("active");
-        $("a[href$=" + href +"]").addClass("active");
-    }
 
 });
