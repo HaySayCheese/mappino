207921311daa94bbd95157fd7ff458a8040403b7
Sergei <SergeiMeketyuk@gmail.com>
2014-07-22 16:08:02 +0300

    fix

--- static/js/main/controllers/mapCtrl.js
+++ static/js/main/controllers/mapCtrl.js
@@ -295,7 +295,11 @@ app.controller('MapCtrl', function($scope, $location, $http, $timeout, $compile,
                         map.setZoom(15);
                     }
 
-                    $scope.filters.map.city = cityInput.value;
+                    $timeout(function() {
+                        cityInput.value = cityInput.value.substring(0, cityInput.value.lastIndexOf(","));
+                        $scope.filters.map.city = cityInput.value;
+                    }, 0);
+
 
                     if(!$scope.$$phase)
                         $scope.$apply();
