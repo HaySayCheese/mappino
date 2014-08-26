2f82094dd1d262898773fc348438fca63f5b37ed
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-08-26 15:46:55 +0300

    fix:

--- core/management/commands/republish_all.py
+++ core/management/commands/republish_all.py
@@ -1,13 +1,16 @@
 from django.core.management import BaseCommand
+from django.db import transaction
+
 from core.publications.constants import HEAD_MODELS
 
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
-		for tid, model in HEAD_MODELS.iteritems():
-			for publication in model.all_published():
-				publication.unpublish()
-				print('publication {0}:{1} unpublished'.format(tid, publication.id))
+		with transaction.atomic():
+			for tid, model in HEAD_MODELS.iteritems():
+				for publication in model.all_published():
+					publication.unpublish()
+					print('publication {0}:{1} unpublished'.format(tid, publication.id))
 
-				publication.publish()
-				print('publication {0}:{1} published back. OK'.format(tid, publication.id))
+					publication.publish()
+					print('publication {0}:{1} published back. OK'.format(tid, publication.id))
