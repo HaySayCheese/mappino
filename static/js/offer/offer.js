5062c12fc29eebbbafef676f7381f48ef3c01869
Dima Chizhevsky <HaySayCheese@gmail.com>
2014-12-29 03:35:54 +0200

    fix: realtors offer page

--- static/js/offer/offer.js
+++ static/js/offer/offer.js
@@ -1,11 +1,11 @@
 "use strict";
 
 $(document).ready(function() {
-
-
     var scrollableBlock = $(document),
         scrollableBlockImage = scrollableBlock.find(".img-holder"),
+        mainImage = scrollableBlock.find(".img-holder.main"),
         headerLinksBlock = scrollableBlock.find(".header-links");
+
 
     scrollableBlockImage.imageScroll({
         container: $('.wrapper'),
@@ -13,51 +13,26 @@ $(document).ready(function() {
     });
 
 
-    //scrollableBlock.scroll(function() {
-    //    var scrollTop = scrollableBlock.scrollTop();
-    //
-    //    setTimeout(function() {
-    //        $(".image-caption")
-    //            .find("h1")
-    //            .css("opacity", (100 / (scrollTop / 7)) - 1.2)
-    //            .parent()
-    //            .find("img")
-    //            .css("opacity", (100 / (scrollTop / 9)) - 2);
-    //    }, 100);
-    //
-    //
-    //
-    //    /** Navbar */
-    //    if (scrollTop > $(window).height() - 70) {
-    //        headerLinksBlock.removeClass("slideOutUp").addClass("slideInDown panel");
-    //    } else if (headerLinksBlock.hasClass("panel") && !headerLinksBlock.hasClass("slideOutUp")) {
-    //            headerLinksBlock
-    //                .removeClass("slideInDown")
-    //                .addClass("slideOutUp")
-    //
-    //                .delay(300).queue(function(next) {
-    //                    $(this).removeClass("slideOutUp panel");
-    //                    next();
-    //                })
-    //                .addClass("slideInDown")
-    //                .delay(250).queue(function(next) {
-    //                    $(this).removeClass("slideInDown");
-    //
-    //                    next();
-    //                });
-    //    }
-    //
-    //    /** Markers */
-    //    var sections = $("section"),
-    //        markers = $(".tablet-marker");
-    //
-    //    if (scrollTop > sections[0].offsetTop - 150) {
-    //        $.each(markers, function(i, el) {
-    //            setTimeout(function() {
-    //                $(el).addClass("fadeInDown");
-    //            }, 300 + (i * 300));
-    //        });
-    //    }
-    //});
+    scrollableBlock.scroll(function() {
+        var scrollTop = scrollableBlock.scrollTop();
 
+        /* Navigation bar logic */
+        if (scrollTop > mainImage.innerHeight() - headerLinksBlock.innerHeight()) {
+            headerLinksBlock.removeClass("slideOutUp").addClass("slideInDown panel");
+
+        } else if (headerLinksBlock.hasClass("panel") && !headerLinksBlock.hasClass("slideOutUp")){
+            headerLinksBlock
+                .removeClass("slideInDown")
+                .addClass("slideOutUp")
+                .delay(300).queue(function(next) {
+                    $(this).removeClass("slideOutUp panel");
+                    next();
+                })
+                .addClass("slideInDown")
+                .delay(250).queue(function(next) {
+                    $(this).removeClass("slideInDown");
+                    next();
+                });
+        }
+    });
 });
