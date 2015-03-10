adf7ee63337781155c14b3b055bcdd0d606a0161
hsc <dima.chizhevsky@gmail.com>
2015-03-10 20:56:02 +0200

    fix

--- apps/main/views.py
+++ apps/main/views.py
@@ -4,12 +4,10 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from core.utils.jinja2_integration import templates
 
 
-
 @ensure_csrf_cookie
 def homepage(request):
     template = templates.get_template('main/home/home.html')
     return HttpResponse(content=template.render())
-
 
 
 @ensure_csrf_cookie
@@ -18,18 +16,18 @@ def map(request):
     return HttpResponse(content=template.render())
 
 
-
+@ensure_csrf_cookie
 def offer(request):
     return HttpResponseRedirect('/offer/realtors/')
 
 
-
+@ensure_csrf_cookie
 def offer_for_realtors(request):
     template = templates.get_template('main/offer/realtors.html')
     return HttpResponse(content=template.render())
 
 
-
+@ensure_csrf_cookie
 def offer_for_agencies(request):
     template = templates.get_template('main/offer/agencies.html')
     return HttpResponse(content=template.render())
