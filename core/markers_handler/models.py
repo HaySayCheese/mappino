8bfb8acebf802e695c43f525e8f631ca79ef52d5
hsc <dima.chizhevsky@gmail.com>
2015-03-13 17:43:46 +0200

    fix:  businesses  index refactoring

--- core/markers_handler/models.py
+++ core/markers_handler/models.py
@@ -1781,6 +1781,14 @@ class WarehousesRentIndex(WarehousesSaleIndex):
 
 
 class AbstractBusinessesIndex(AbstractBaseIndex):
+    """
+    Sale index and rent index of the businesses contains the same field and the same logic,
+    but must use different tables. For this approach we can't simply create sale index,
+    and than inherit rent index from it. (Django will generate inappropriate inheritance scheme).
+
+    So abstract index was developed and both sale and rent indexes was derived from it.
+    """
+
     price = models.FloatField(db_index=True)
     currency_sid = models.PositiveSmallIntegerField()
 
