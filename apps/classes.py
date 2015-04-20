599dcef5069bcfc4110a8b500b5efb7cdbd8ffb3
hsc <dima.chizhevsky@gmail.com>
2015-04-20 09:22:36 +0300

    refactoring: common libraries

--- apps/classes.py
+++ apps/classes.py
@@ -1,4 +1,4 @@
-from collective.decorators.views import anonymous_require, login_required_or_forbidden
+from collective.decorators.views import anonymous_required, login_required_or_forbidden
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 
@@ -9,7 +9,7 @@ class AnonymousOnlyView(View):
     Base class for all anonymous only views.
     The only purpose of using this view is to reject all authenticated requests.
     """
-    @method_decorator(anonymous_require)
+    @method_decorator(anonymous_required)
     def dispatch(self, *args, **kwargs):
         return super(AnonymousOnlyView, self).dispatch(*args, **kwargs)
 
