1ccacd21cf7052462a1f71af38d866e734e5391e
hsc <dima.chizhevsky@gmail.com>
2015-05-26 15:50:05 +0300

    fix

--- apps/cabinet/templates_ajax/publications.py
+++ apps/cabinet/templates_ajax/publications.py
@@ -25,45 +25,59 @@ def briefs_template(request):
 @ensure_csrf_cookie
 # @condition(last_modified_func=static_template_last_modified)
 def unpublished_form_template(request, tid):
-	if not tid:
-		return HttpResponseBadRequest('@tid is invalid')
-	try:
-		tid = int(tid)
-	except ValueError:
-		return HttpResponseBadRequest('@tid is invalid')
+	tid = int(tid)
 
-	# Жилая недвижимость
+
+	# living realty
 	if tid == OBJECTS_TYPES.house():
-		return HttpResponse(templates.get_template(
-			'cabinet/publications/unpublished/house.html').render())
+		return HttpResponse(
+            templates.get_template('cabinet/publications/unpublished/house.html').render()
+        )
+
 	elif tid == OBJECTS_TYPES.flat():
-		return HttpResponse(templates.get_template(
-			'cabinet/publications/unpublished/flat.html').render())
+		return HttpResponse(
+            templates.get_template('cabinet/publications/unpublished/flat.html').render()
+        )
+
 	elif tid == OBJECTS_TYPES.room():
-		return HttpResponse(templates.get_template(
-			'cabinet/publications/unpublished/room.html').render())
+		return HttpResponse(
+            templates.get_template('cabinet/publications/unpublished/room.html').render()
+        )
 
-	# Коммерческая недвижимость
+
+	# commercial realty
 	elif tid == OBJECTS_TYPES.trade():
-		return HttpResponse(templates.get_template(
-			'cabinet/publications/unpublished/trade.html').render())
-	elif tid == OBJECTS_TYPES.office():
-		return HttpResponse(templates.get_template(
-			'cabinet/publications/unpublished/office.html').render())
-	elif tid == OBJECTS_TYPES.warehouse():
-		return HttpResponse(templates.get_template(
-			'cabinet/publications/unpublished/warehouse.html').render())
-	elif tid == OBJECTS_TYPES.business():
-		return HttpResponse(templates.get_template(
-			'cabinet/publications/unpublished/business.html').render())
+		return HttpResponse(
+            templates.get_template('cabinet/publications/unpublished/trade.html').render()
+        )
 
-	# Другая недвижимость
+	elif tid == OBJECTS_TYPES.office():
+		return HttpResponse(
+            templates.get_template('cabinet/publications/unpublished/office.html').render()
+        )
+
+	elif tid == OBJECTS_TYPES.warehouse():
+		return HttpResponse(
+            templates.get_template('cabinet/publications/unpublished/warehouse.html').render()
+        )
+
+	elif tid == OBJECTS_TYPES.business():
+		return HttpResponse(
+            templates.get_template('cabinet/publications/unpublished/business.html').render()
+        )
+
+
+	# other
 	elif tid == OBJECTS_TYPES.garage():
-		return HttpResponse(templates.get_template(
-			'cabinet/publications/unpublished/garage.html').render())
+		return HttpResponse(
+            templates.get_template('cabinet/publications/unpublished/garage.html').render()
+        )
+
 	elif tid == OBJECTS_TYPES.land():
-		return HttpResponse(templates.get_template(
-			'cabinet/publications/unpublished/land.html').render())
+		return HttpResponse(
+            templates.get_template('cabinet/publications/unpublished/land.html').render()
+        )
+
 
 	return HttpResponseBadRequest('@tid is invalid.')
 
