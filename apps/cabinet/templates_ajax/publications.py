be21ee4946152eed5767137de9620657ee16c420
hsc <dima.chizhevsky@gmail.com>
2015-05-26 15:59:50 +0300

    fix

--- apps/cabinet/templates_ajax/publications.py
+++ apps/cabinet/templates_ajax/publications.py
@@ -10,10 +10,10 @@ from core.utils.jinja2_integration import templates
 @ensure_csrf_cookie
 # @condition(last_modified_func=static_template_last_modified)
 def briefs_template(request):
-	t = templates.get_template('cabinet/_common/publications/briefs.html')
-	return HttpResponse(t.render())
+    t = templates.get_template('cabinet/_common/publications/briefs.html')
+    return HttpResponse(t.render())
 
-#
+
 # @ensure_csrf_cookie
 # @condition(last_modified_func=static_template_last_modified)
 # def publications_panel_template(request):
@@ -25,85 +25,85 @@ def briefs_template(request):
 @ensure_csrf_cookie
 # @condition(last_modified_func=static_template_last_modified)
 def unpublished_form_template(request, tid):
-	tid = int(tid)
+    tid = int(tid)
 
 
-	# living realty
-	if tid == OBJECTS_TYPES.house():
-		return HttpResponse(
+    # living realty
+    if tid == OBJECTS_TYPES.house():
+        return HttpResponse(
             templates.get_template('cabinet/_common/publications/unpublished/house.html').render()
         )
 
-	elif tid == OBJECTS_TYPES.flat():
-		return HttpResponse(
+    elif tid == OBJECTS_TYPES.flat():
+        return HttpResponse(
             templates.get_template('cabinet/_common/publications/unpublished/flat.html').render()
         )
 
-	elif tid == OBJECTS_TYPES.room():
-		return HttpResponse(
+    elif tid == OBJECTS_TYPES.room():
+        return HttpResponse(
             templates.get_template('cabinet/_common/publications/unpublished/room.html').render()
         )
 
 
-	# commercial realty
-	elif tid == OBJECTS_TYPES.trade():
-		return HttpResponse(
+    # commercial realty
+    elif tid == OBJECTS_TYPES.trade():
+        return HttpResponse(
             templates.get_template('cabinet/_common/publications/unpublished/trade.html').render()
         )
 
-	elif tid == OBJECTS_TYPES.office():
-		return HttpResponse(
+    elif tid == OBJECTS_TYPES.office():
+        return HttpResponse(
             templates.get_template('cabinet/_common/publications/unpublished/office.html').render()
         )
 
-	elif tid == OBJECTS_TYPES.warehouse():
-		return HttpResponse(
+    elif tid == OBJECTS_TYPES.warehouse():
+        return HttpResponse(
             templates.get_template('cabinet/_common/publications/unpublished/warehouse.html').render()
         )
 
-	elif tid == OBJECTS_TYPES.business():
-		return HttpResponse(
+    elif tid == OBJECTS_TYPES.business():
+        return HttpResponse(
             templates.get_template('cabinet/_common/publications/unpublished/business.html').render()
         )
 
 
-	# other realty
-	elif tid == OBJECTS_TYPES.garage():
-		return HttpResponse(
+    # other realty
+    elif tid == OBJECTS_TYPES.garage():
+        return HttpResponse(
             templates.get_template('cabinet/_common/publications/unpublished/garage.html').render()
         )
 
-	elif tid == OBJECTS_TYPES.land():
-		return HttpResponse(
+    elif tid == OBJECTS_TYPES.land():
+        return HttpResponse(
             templates.get_template('cabinet/_common/publications/unpublished/land.html').render()
         )
 
 
-	return HttpResponseBadRequest('@tid is invalid.')
-
+    return HttpResponseBadRequest('@tid is invalid.')
 
 
 @ensure_csrf_cookie
-@condition(last_modified_func=static_template_last_modified)
+# @condition(last_modified_func=static_template_last_modified)
 def unpublished_map_template(request):
-	t = templates.get_template('cabinet/publications/_common/unpublished/parts/map.html')
-	return HttpResponse(t.render())
-
+    return HttpResponse(
+        templates.get_template('cabinet/publications/_common/unpublished/parts/map.html')
+    )
 
 
 @ensure_csrf_cookie
-@condition(last_modified_func=static_template_last_modified)
+# @condition(last_modified_func=static_template_last_modified)
 def unpublished_photos_template(request):
-	t = templates.get_template('cabinet/publications/_common/unpublished/parts/photos.html')
-	return HttpResponse(t.render())
-
+    return HttpResponse(
+        templates.get_template('cabinet/publications/_common/unpublished/parts/photos.html')
+    )
 
 
 @ensure_csrf_cookie
-@condition(last_modified_func=static_template_last_modified)
+# @condition(last_modified_func=static_template_last_modified)
 def published_form_template(request):
-	t = templates.get_template('cabinet/publications/_common/published/published.html')
-	return HttpResponse(t.render())
+    return HttpResponse(
+        templates.get_template('cabinet/publications/_common/published/published.html')
+    )
 
 
 
