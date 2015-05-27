5f95b19c30f4d9e14730b791a8b1511008a27fa1
hsc <dima.chizhevsky@gmail.com>
2015-05-27 11:05:52 +0300

    fix

--- apps/cabinet/views.py
+++ apps/cabinet/views.py
@@ -1,22 +1,28 @@
 # coding=utf-8
 from django.http import HttpResponse, HttpResponseRedirect
+from django.http.response import HttpResponseForbidden
 from django.views.decorators.csrf import ensure_csrf_cookie
 from core.utils.jinja2_integration import templates
 
 
 @ensure_csrf_cookie
-def main(request):
-	# if not request.user.is_authenticated():
-	# 	return HttpResponseRedirect('/map/#!/account/login')
+def cabinet(request):
+    user = request.user
+    if not user.is_authenticated():
+        return HttpResponseForbidden()
 
-	template = templates.get_template('cabinet/users/users.html')
-	return HttpResponse(content=template.render())
+    # permissions check
+    if not user.is_staff and not user.is_superuser:
+        return HttpResponseForbidden()
+
+    template = templates.get_template('cabinet/users/users.html')
+    return HttpResponse(content=template.render())
 
 
 @ensure_csrf_cookie
 def login(request):
-	# if not request.user.is_authenticated():
-	# 	return HttpResponseRedirect('/map/#!/account/login')
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/cabinet/')
 
-	template = templates.get_template('cabinet/login.html')
-	return HttpResponse(content=template.render())
+    template = templates.get_template('cabinet/login.html')
+    return HttpResponse(content=template.render())
