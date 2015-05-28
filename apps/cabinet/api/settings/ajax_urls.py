78825a04c1b3928b9d256f5e2f6638ec7e1a1ca5
hsc <dima.chizhevsky@gmail.com>
2015-05-28 14:04:28 +0300

    fix

--- apps/cabinet/api/settings/ajax_urls.py
+++ apps/cabinet/api/settings/ajax_urls.py
@@ -1,8 +1,8 @@
-from apps.cabinet.api.settings.ajax import AccountManager
+from apps.cabinet.api.settings.ajax import AccountView
 from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('',
-    url(r'^ajax/api/cabinet/account/$', AccountManager.AccountView.as_view()),
-    url(r'^ajax/api/cabinet/account/photo/$', AccountManager.AvatarUpdate.as_view()),
+    url(r'^ajax/api/cabinet/account/$', AccountView.as_view()),
+    # url(r'^ajax/api/cabinet/account/photo/$', AvatarUpdate.as_view()),
 )
