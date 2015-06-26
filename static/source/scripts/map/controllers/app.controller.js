68f88de7c10342fac6e4d2f09df432a804558149
serhii <serhii.meketiuk@gmail.com>
2015-06-26 18:11:52 +0300

    fixed

--- static/source/scripts/map/controllers/app.controller.js
+++
@@ -1,7 +0,0 @@
-export class AppController {
-    constructor($state, $rootScope, $location, panelsHandler) {
-
-    }
-}
-
-AppController.$inject = ['$state', '$rootScope', '$location', 'PanelsHandler'];
