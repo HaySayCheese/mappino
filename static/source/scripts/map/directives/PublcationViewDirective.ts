02e0ec96b73af5cf5cef609b912cce7bf8ebaa2e
serhii <serhii.meketiuk@gmail.com>
2015-07-07 17:04:28 +0300

    fixed directives style and code

--- static/source/scripts/map/directives/PublcationViewDirective.ts
+++ static/source/scripts/map/directives/PublcationViewDirective.ts
@@ -34,55 +34,6 @@ module pages.map {
                         }
                     }
                 });
-
-
-                //if (publicationHandler.isOpened()) {
-                //    if (navbarsHandler.isOpened('navbar_right')) {
-                //        openPublicationWithNavbarRight();
-                //    } else {
-                //        openPublication();
-                //    }
-                //} else {
-                //    closePublication();
-                //}
-                //
-                //
-                //
-                //
-                //scope.$on('PublicationHandler.Open', openPublication);
-                //scope.$on('PublicationHandler.OpenWithNavbarRight', openPublicationWithNavbarRight);
-                //scope.$on('PublicationHandler.Close', closePublication);
-                //
-                //
-                //var openPublication = () => {
-                //    angular.element(element)
-                //        .removeClass('-closed')
-                //        .removeClass('-with-navbar-right')
-                //        .addClass('-opened');
-                //    console.log('opened')
-                //
-                //};
-                //
-                //
-                //var openPublicationWithNavbarRight = () => {
-                //    angular.element(element)
-                //        .removeClass('-opened')
-                //        .removeClass('-closed')
-                //        .addClass('-with-navbar-right');
-                //
-                //    console.log('with')
-                //};
-                //
-                //
-                //var closePublication = () => {
-                //    angular.element(element)
-                //        .removeClass('-opened')
-                //        .removeClass('-with-navbar-right')
-                //        .addClass('-closed');
-                //    console.log('closed')
-                //
-                //};
-
             }
         };
 
