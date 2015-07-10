0bf4909c3a408b84acba086117e3a2347de96a80
hsc <dima.chizhevsky@gmail.com>
2015-07-11 00:34:15 +0300

    favorites: refactored views

--- apps/main/api/favorites/ajax.py
+++ apps/main/api/favorites/ajax.py
@@ -1,185 +1,133 @@
 # coding=utf-8
-from core.favorites.exceptions import InvalidCustomer
-from django.core.exceptions import ObjectDoesNotExist
-from django.views.generic import View
-
+from apps.classes import AuthenticatedOnlyView
+from collective.decorators.ajax import json_response
 from collective.http.responses import *
 from collective.methods.request_data_getters import angular_parameters
-from core.customers.models import Customers
 from core.favorites.models import Favorites
 from core.publications.models import HEAD_MODELS
 
 
-class FavoritesBaseView(object):
-    # This cookie is used to distinguish customers one from another.
-    # The value in this cookie is the hash digest that uniquely identifies the customer.
-    customer_hash_id_cookie_name = 'customer_hash_id'
-
-    class CommonResponses(object):
-        """
-        Contains common response serializers for several descendant views.
-        """
+class FavoritesListView(AuthenticatedOnlyView):
+    class GetResponses(object):
         @staticmethod
-        def empty_customer_hash_id_cookie():
-            return HttpJsonResponseBadRequest({
+        @json_response
+        def ok(favorites_list):
+            return {
+                'code': 0,
+                'message': 'OK',
+                'data': [record for record in favorites_list]
+            }
+
+
+        @staticmethod
+        @json_response
+        def anonymous_user():
+            return {
                 'code': 1,
-                'message': "Unauthorized user. Customer cookie is missed."
-            })
+                'message': 'User must be authenticated.'
+            }
+
+
+    class PostResponses(object):
+        @staticmethod
+        @json_response
+        def ok():
+            return {
+                'code': 0,
+                'message': 'OK'
+            }
+
 
         @staticmethod
-        def invalid_customers_hash_id():
-            return HttpJsonResponseBadRequest({
-                'code': 2,
-                'message': "Invalid customer id. There is no customers with this customers hash id."
-            })
+        @json_response
+        def invalid_params():
+            return {
+                'code': 1,
+                'message': 'Request does not contains valid parameters.'
+            }
 
+
+    class DeleteResponses(object):
         @staticmethod
-        def absent_publications_id():
-            return HttpJsonResponseBadRequest({
-                'code': 3,
-                'message': '"tid" or "hash_id" for publication is absent.'
-            })
-
-
-class FavoritesListView(FavoritesBaseView, View):
-    @classmethod
-    def get(cls, request, *args):
-        try:
-            customer_hash_id = request.get_signed_cookie(cls.customer_hash_id_cookie_name)
-        except Exception:
-            return cls.CommonResponses.empty_customer_hash_id_cookie()
-
-        try:
-            favorites = Favorites.objects.all()
-            print favorites.count()
-            favorite = Favorites.objects.filter(customer__hash_id=customer_hash_id)[:1][0]
-        except IndexError:
-            return cls.CommonResponses.invalid_customers_hash_id()
-
-
-        publications_info = cls.get_information_about_publications(favorite)
-        return cls.Get.ok(publications_info)
+        @json_response
+        def ok():
+            return {
+                'code': 0,
+                'message': 'OK'
+            }
 
 
     @classmethod
-    def post(cls, request, *args):
-        try:
-            customer_hash_id = request.get_signed_cookie(cls.customer_hash_id_cookie_name)
-        except Exception:
-            return cls.CommonResponses.empty_customer_hash_id_cookie()
+    def get(cls, request):
+        """
+        :returns:
+            All the favorites of the authenticated user.
+        """
+        record_with_favorites = Favorites.by_user(request.user.id)
+        if not record_with_favorites:
+            # user does not have favorites,
+            # lets return empty response
+            return cls.GetResponses.ok([])
 
-        try:
-            customer_id = Customers.objects.filter(hash_id=customer_hash_id).values_list('id')[0][0]
-        except ObjectDoesNotExist:
-            return cls.CommonResponses.invalid_customers_hash_id()
 
+        favorites_info = cls.__get_information_about_publications(record_with_favorites)
+        return cls.GetResponses.ok(favorites_info)
+
+
+    @classmethod
+    def post(cls, request):
+        """
+        Adds publication to the user's favorites.
+        """
         try:
-            params = angular_parameters(request, ['id'])
-            tid, hash_id = params['id'].split(':')
+            publication_id = angular_parameters(request, ['publication_id'])['publication_id']
+            tid, hash_id = publication_id['id'].split(':')
             tid = int(tid)
         except ValueError:
-            return cls.CommonResponses.absent_publications_id()
+            return cls.PostResponses.invalid_params()
 
 
-        favorite = Favorites.objects.get_or_create(customer_id=customer_id)[0]
-        favorite.add(customer_id, tid, hash_id)
-        return cls.Post.ok()
+        Favorites.add(request.user, tid, hash_id)
+        return cls.PostResponses.ok()
 
 
-    #todo  optimize request/ GEt all publication of one type
     @classmethod
-    def get_information_about_publications(cls,favorite):
-        publications_ids = json.loads(favorite.publications_ids)
-        list_with_publications_ids = [publication_ids.split(":",2) for publication_ids in publications_ids]
-        list_with_publications_ids = [[int(publication_ids[0]),publication_ids[1]] for publication_ids in list_with_publications_ids]
-        list_with_publications_info = []
-        for publication_ids in list_with_publications_ids:
-            publication_model = HEAD_MODELS[publication_ids[0]]
-            try:
-                publication = publication_model.objects.filter(hash_id = publication_ids[1]).only('body__title')[:1][0]
-            except IndexError:
-                return cls.Get.publication_does_not_exist(json.dumps(publications_ids))
+    def delete(cls, request, tid, hash_id):
+        """
+        Removes publication with of type "tid" and hash = "hash_id" from the user's favorites.
+        """
+        Favorites.remove(request.user.id, tid, hash_id)
+        return cls.DeleteResponses.ok()
 
-            photos = publication.photos().only('big_thumb_url')
-            big_thumb_urls = [photo.big_thumb_url for photo in photos]
-            list_with_publications_info.append(
-                {
-                    'ids':"{tid}:{hid}".format(publication_ids[0],publication_ids[1]),
-                    'photos':big_thumb_urls,
-                    'title':publication.photos
-                })
+
+    @classmethod
+    def __get_information_about_publications(cls, record_with_favorites):
+        """
+        :returns:
+            Serializes and returns brief information about favorite publications.
+
+            This method is not optimized, but it is not necessary,
+            because the average user will not have more than 10 favorite publications.
+        """
+        publications_ids = json.loads(record_with_favorites.publications_ids)
+
+        list_with_publications_ids = [publication_ids.split(":",2) for publication_ids in publications_ids]
+        list_with_publications_ids = [
+            (int(publication_ids[0]), publication_ids[1]) for publication_ids in list_with_publications_ids
+        ]
+
+        list_with_publications_info = []
+        for tid, hash_id in list_with_publications_ids:
+            model = HEAD_MODELS[tid]
+            publication = model.objects.filter(hash_id=hash_id).only('body__title')[:1][0]
+
+            # photos = publication.photos().only('big_thumb_url')
+            # big_thumb_urls = [photo.big_thumb_url for photo in photos]
+            list_with_publications_info.append({
+                'id': "{tid}:{hid}".format(tid, hash_id),
+                'title': publication.body.title,
+                #'photos':big_thumb_urls,
+
+            })
 
         return list_with_publications_info
-
-
-    class Get(object):
-
-        @staticmethod
-        def publication_does_not_exist(publications_ids):
-            return HttpJsonResponse({
-                'code':1,
-                'message': "Publication with {0} does not exist".format(publications_ids)
-            })
-
-        @staticmethod
-        def ok(pubclications_info):
-            return HttpJsonResponse({
-                'code': 0,
-                'message': 'OK',
-                'data': [pubclication_info for pubclication_info in pubclications_info]
-            })
-
-    class Post(object):
-        @staticmethod
-        def ok():
-            return HttpJsonResponse({
-                'code': 0,
-                'message': 'OK',
-                })
-
-
-class FavoritesView(FavoritesBaseView, View):
-    @classmethod
-    def delete(cls, request, *args):
-        tid, hash_id = int(args[0]), args[1]
-
-        try:
-            customer_hash_id = request.get_signed_cookie(cls.customer_hash_id_cookie_name)
-        except Exception:
-            return cls.CommonResponses.empty_customer_hash_id_cookie()
-
-        try:
-            customer_id = Customers.objects.filter(hash_id=customer_hash_id).values_list('id')[0][0]
-        except IndexError:
-            return cls.CommonResponses.invalid_customers_hash_id()
-
-
-        favorite = Favorites.objects.get_or_create(customer_id=customer_id)[0]
-
-        try:
-            if favorite.remove(customer_id, tid, hash_id):
-                return cls.Delete.ok()
-            else:
-                return cls.Delete.publication_does_not_exist(tid, hash_id)
-
-        except InvalidCustomer:
-            return cls.CommonResponses.invalid_customers_hash_id()
-
-
-    class Delete(object):
-        @staticmethod
-        def ok():
-            return HttpJsonResponse({
-                'code': 0,
-                'message': 'OK',
-            })
-
-        @staticmethod
-        def publication_does_not_exist(tid, hash_id):
-            response = HttpJsonResponseNotFound({
-                'code': 1,
-                'message': "Publication with id='{tid}:{hash_id}' is not in favorites.".format(tid=tid, hash_id=hash_id)
-            })
-            response.delete_cookie('customer_hash_id')
-            return response
-
