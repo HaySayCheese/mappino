16e992d0ba4adce73c27949aa898482bcae92efd
hsc <dima.chizhevsky@gmail.com>
2015-07-14 17:45:20 +0300

    fix

--- apps/cabinet/api/support/web_hooks.py
+++ apps/cabinet/api/support/web_hooks.py
@@ -13,69 +13,71 @@ from core.support.models import Tickets
 
 
 class IncomingAgentResponseHook(View):
-	def get(self, request, *args):
-		"""
-		Mandrill check's the the web hook before applying it,
-		so we need to return code 200 to it.
-		"""
-		return HttpResponse()
+    @staticmethod
+    def get():
+        """
+        Mandrill check's the the web hook before applying it,
+        so we need to return code 200 to it.
+        """
+        return HttpResponse()
 
 
-	def post(self, request, *args):
-		try:
-			events = POST_parameter(request, 'mandrill_events')
-		except ValueError:
-			return HttpResponseBadRequest('No parameter @mandrill_events in request.')
+    @staticmethod
+    def post(request):
+        try:
+            events = POST_parameter(request, 'mandrill_events')
+        except ValueError:
+            return HttpResponseBadRequest('No parameter @mandrill_events in request.')
 
-		events = json.loads(events)
-		if not events:
-			return HttpResponseBadRequest('Empty @mandrill_events is not allowed.')
-
-
-		with transaction.atomic():
-			for event in events:
-				# subject
-				subject = event['msg'].get('subject', '')
-				if not subject:
-					raise ValueError('Empty subject.')
-
-				if 'Re: ' in subject:
-					subject = subject[4:]
+        events = json.loads(events)
+        if not events:
+            return HttpResponseBadRequest('Empty @mandrill_events is not allowed.')
 
 
-				# message = event['msg'].get('text', '')
-				message = event['msg'].get('html', '')
-				if not message:
-					raise ValueError('Empty message.')
+        with transaction.atomic():
+            for event in events:
+                # subject
+                subject = event['msg'].get('subject', '')
+                if not subject:
+                    raise ValueError('Empty subject.')
+
+                if 'Re: ' in subject:
+                    subject = subject[4:]
 
 
-				try:
-					# Пробуємо розпарсити і відформатувати відповідь, припускаючи, що вона надійшла від gmail.
-					message = message[:message.index('<div class="gmail_quote">')]
-
-					extra = message[message.index('<div class="gmail_extra">'):]
-					extra = extra.replace('<br>','')
-					extra = extra.replace('\r','')
-					extra = extra.replace('\n','')
-					extra = extra.replace('<br clear="all">','<br/>')
-					message = message[:message.index('<div class="gmail_extra">')] + extra
-				except:
-					# Розпарсити не вдається.
-					# Отже відповідь надійшла з yandex і її парсити не треба.
-					pass
+                # message = event['msg'].get('text', '')
+                message = event['msg'].get('html', '')
+                if not message:
+                    raise ValueError('Empty message.')
 
 
-				# Searching the ticket and adding the agent's response.
-				try:
-					ticket_id = (subject[7:]).split(':')[0]
-					ticket_id = int(ticket_id)
-				except ValueError:
-					raise RuntimeException('Invalid subject: {0}'.format(subject))
+                try:
+                    # Пробуємо розпарсити і відформатувати відповідь, припускаючи, що вона надійшла від gmail.
+                    message = message[:message.index('<div class="gmail_quote">')]
 
-				try:
-					ticket = Tickets.objects.get(id=ticket_id)
-				except ObjectDoesNotExist:
-					raise RuntimeException('Invalid ticket')
+                    extra = message[message.index('<div class="gmail_extra">'):]
+                    extra = extra.replace('<br>','')
+                    extra = extra.replace('\r','')
+                    extra = extra.replace('\n','')
+                    extra = extra.replace('<br clear="all">','<br/>')
+                    message = message[:message.index('<div class="gmail_extra">')] + extra
+                except:
+                    # Розпарсити не вдається.
+                    # Отже відповідь надійшла з yandex і її парсити не треба.
+                    pass
 
-				ticket.add_support_answer(message)
-		return HttpResponse()
+
+                # Searching the ticket and adding the agent's response.
+                try:
+                    ticket_id = (subject[7:]).split(':')[0]
+                    ticket_id = int(ticket_id)
+                except ValueError:
+                    raise RuntimeException('Invalid subject: {0}'.format(subject))
+
+                try:
+                    ticket = Tickets.objects.get(id=ticket_id)
+                except ObjectDoesNotExist:
+                    raise RuntimeException('Invalid ticket')
+
+                ticket.add_support_answer(message)
+        return HttpResponse()
