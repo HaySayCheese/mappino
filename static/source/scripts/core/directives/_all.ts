6eaea193e946d3fe30ac5cbd2e7c951eb9e32b91
serhii <serhii.meketiuk@gmail.com>
2015-07-17 12:29:15 +0300

    fixed

--- static/source/scripts/core/directives/_all.ts
+++ static/source/scripts/core/directives/_all.ts
@@ -1,3 +1,4 @@
 /// <reference path='directives/only-number.directive.ts' />
+/// <reference path='directives/only-number-with-dots.directive.ts' />
 /// <reference path='directives/on-src-error.directive.ts' />
 /// <reference path='directives.module.ts' />
