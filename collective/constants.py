82c260f67e5f51cf4713832a40fd81ed5f50ca6b
hsc <dima.chizhevsky@gmail.com>
2015-07-23 14:37:06 +0300

    fix

--- collective/constants.py
+++ collective/constants.py
@@ -1,19 +1,29 @@
+#coding=utf-8
+
+
 class AbstractConstant(object):
-	def __init__(self):
-		self.ids = {}
+    def __init__(self):
+        self.ids = {}
+        self.count = 0
 
-	def set_ids(self, ids_dict):
-		if len(set(ids_dict.values())) < len(ids_dict.values()):
-			raise ValueError('Duplicate id detected.')
-		self.ids = ids_dict
-		self.count = len(self.ids.keys())
 
-	# system functions
-	def records(self):
-		return self.ids
+    def set_ids(self, ids_dict):
+        # check for duplicates
+        if len(set(ids_dict.values())) < len(ids_dict.values()):
+            raise ValueError('Duplicate id detected.')
 
-	def values(self):
-		return self.ids.values()
+        self.ids = ids_dict
+        self.count = len(self.ids.keys())
 
-	def keys(self):
-		return self.ids.keys()
+
+    # system functions
+    def records(self):
+        return self.ids
+
+
+    def values(self):
+        return self.ids.values()
+
+
+    def keys(self):
+        return self.ids.keys()
