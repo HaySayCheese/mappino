c67830a78e6d9766358798dbb766e75d3423b279
hsc <dima.chizhevsky@gmail.com>
2015-07-24 11:14:04 +0300

    fix:

--- core/publications/formatters.py
+++ core/publications/formatters.py
@@ -291,60 +291,39 @@ class UnpublishedFormatter(object):
     @classmethod
     def format(cls, tid, record):
         head = serializers.serialize(
-            'python', [record], fields=('created', 'actual', 'for_rent', 'for_sale', 'state_sid',
-                                        'degree_lat', 'degree_lng', 'segment_lat', 'segment_lng',
-                                        'pos_lat', 'pos_lng','address'))[0]['fields']
-
-        # Переформатувати дату створення оголошення у прийнятний для десериалізації формат
-        created = head['created']
-        if created is not None:
-            head['created'] = created.isoformat()
-
-        # Переформатувати дату завершального терміну актуальності оголошення
-        # у прийнятний для десериалізації формат
-        actual = head['actual']
-        if actual is not None:
-            head['actual'] = actual.isoformat()
+            'python', [record], fields=(
+                'created', 'for_rent', 'for_sale', 'state_sid', 'degree_lat', 'degree_lng',
+                'segment_lat', 'segment_lng', 'pos_lat', 'pos_lng', 'address'
+            )
+        )[0]['fields']
 
 
         body = serializers.serialize('python', [record.body])[0]['fields']
-
-        # Якщо оголошення призначено для продажу - підгрузити і сериалізувати цю інформацію.
-        # Дана інформація не грузиться автоматично щоб уникнути потенційно-зайвих селектів.
-        if record.for_sale:
-            sale_terms = serializers.serialize('python', [record.sale_terms])[0]['fields']
-        else:
-            sale_terms = None
-
-        # Якщо оголошення призначено для оренди - підгрузити і сериалізувати цю інформацію.
-        # Дана інформація не грузиться автоматично щоб уникнути потенційно-зайвих селектів.
-        if record.for_rent:
-            rent_terms = serializers.serialize('python', [record.rent_terms])[0]['fields']
-        else:
-            rent_terms = None
+        sale_terms = serializers.serialize('python', [record.sale_terms])[0]['fields']
+        rent_terms = serializers.serialize('python', [record.rent_terms])[0]['fields']
 
         data = {
             'head': head,
             'body': body,
             'sale_terms': sale_terms,
             'rent_terms': rent_terms,
-            'photos': [{
-                'thumbnail_url': photo.big_thumb_url,
-                'photo_url': photo.photo_url,
-                'is_title': photo.check_is_title(),
-                'hash_id': photo.hash_id,
-            } for photo in record.photos()]
+            'photos': [
+                {
+                    'thumbnail_url': photo.big_thumb_url,
+                    'photo_url': photo.photo_url,
+                    'is_title': photo.check_is_title(),
+                    'hash_id': photo.hash_id,
+                } for photo in record.photos()
+            ]
         }
         return cls.format_output_data(data)
 
 
     @classmethod
     def format_output_data(cls, data):
-        # maps coordinates
-        head = data.get('head')
-        if head is None:
-            raise ValueError('@head can not be None.')
+        head = data['head']
 
+        # maps coordinates
         degree_lat = head.get('degree_lat')
         degree_lng = head.get('degree_lng')
         if (degree_lat is None) or (degree_lng is None):
@@ -407,31 +386,6 @@ class UnpublishedFormatter(object):
                 else:
                     # Інакше - округлити до 2х знаків після коми
                     data['rent_terms']['price'] = "%.2f" % r_price
-
-        # Костиль..
-        # Об’єкти готового бізнесу мають 2 decimal-поля, які ломають json-encoder.
-        # Логічно винести функцію формування json-опису об’єкту в модель,
-        # але зараз зроблено як зроблено і часу змінювати це немає.
-        # Отже, тут ці 2 поля переформатовуються.
-
-        monthly_costs = data['body'].get('monthly_costs')
-        if monthly_costs:
-            if int(monthly_costs) == monthly_costs:
-                # Якщо після коми лише нулі - повернути ціле значення
-                data['body']['monthly_costs'] = "%.0f" % monthly_costs
-            else:
-                # Інакше - округлити до 2х знаків після коми
-                data['body']['monthly_costs'] = "%.2f" % float(monthly_costs)
-
-
-        annual_receipts = data['body'].get('annual_receipts')
-        if annual_receipts:
-            if int(annual_receipts) == annual_receipts:
-                # Якщо після коми лише нулі - повернути ціле значення
-                data['body']['annual_receipts'] = "%.0f" % annual_receipts
-            else:
-                # Інакше - округлити до 2х знаків після коми
-                data['body']['annual_receipts'] = "%.2f" % float(annual_receipts)
 
 
         return data
