21355f50cc51170715658819f5425d113ea33976
hsc <dima.chizhevsky@gmail.com>
2015-08-03 13:57:01 +0300

    fix: contacts templates

--- apps/main/templates/urls.py
+++ apps/main/templates/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('apps.main.templates.templates',
     url(r'^ajax/template/map/filters/(\w+)/(\d+)/$', 'filters_form_by_tid'),
 
     url(r'^ajax/template/map/publication/view/$', 'publication_view'),
+    url(r'^ajax/template/map/publication/seller-contacts/$', 'seller_contacts'),
     url(r'^ajax/template/map/publication/detailed/(\d+)/$', 'publication_detailed'),
 )
