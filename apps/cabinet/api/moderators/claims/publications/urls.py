adeb29c03905ed6c26c552272030945b34115c8f
hsc <dima.chizhevsky@gmail.com>
2015-08-11 11:05:50 +0300

    fix

--- apps/cabinet/api/moderators/claims/publications/urls.py
+++ apps/cabinet/api/moderators/claims/publications/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from apps.cabinet.api.moderators.claims.publications.ajax import PublicationView, NextPublicationToCheckView, \
     PublicationAcceptRejectOrHoldView
 
+
 urlpatterns = patterns('',
     url(r'^ajax/api/moderators/publications/next/$', NextPublicationToCheckView.as_view()),
     url(r'^ajax/api/moderators/publications/(\d+):(\w+)/$', PublicationView.as_view()),
