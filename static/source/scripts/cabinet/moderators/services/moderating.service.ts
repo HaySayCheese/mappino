a08268764ed69416f5518b3397d75d4034238514
natakopulenko <nata.kopulenko@mail.ru>
2015-08-19 16:06:52 +0300

    fixed

--- static/source/scripts/cabinet/moderators/services/moderating.service.ts
+++ static/source/scripts/cabinet/moderators/services/moderating.service.ts
@@ -27,174 +27,121 @@ namespace Mappino.Cabinet.Moderators {
 
 
 
-        public getPublicationId(successCallback?, errorCallback?) {
-            this.$http.get(`/ajax/api/moderators/publications/next/`)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        angular.isFunction(successCallback) && successCallback(response.data)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.LOAD.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+        public getPublicationId(): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.get(`/ajax/api/moderators/publications/next/`);
+            promise.success(response => {});
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.LOAD.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+            return promise;
+
         }
 
 
 
-        public load(publicationIds: any, successCallback?, errorCallback?) {
-            this.$http.get(`/ajax/api/moderators/publications/${publicationIds.tid}:${publicationIds.hid}/`)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        angular.isFunction(successCallback) && successCallback(response.data)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.LOAD.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+        public load(publicationIds: any): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.get(`/ajax/api/moderators/publications/${publicationIds.tid}:${publicationIds.hid}/`);
+            promise.success(response => {});
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.LOAD.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+            return promise;
         }
 
 
 
-        public loadHeld(successCallback?, errorCallback?) {
-            this.$http.get(`/ajax/api/moderators/publications/held/briefs/`)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        angular.isFunction(successCallback) && successCallback(response.data)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.LOAD.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+        public loadHeld(): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.get(`/ajax/api/moderators/publications/held/briefs/`);
+            promise.success(response => {});
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.LOAD.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+            return promise;
         }
 
 
 
-        public accept(publicationIds: any, successCallback?, errorCallback?) {
-            this.$http.post(`/ajax/api/moderators/publications/${publicationIds.tid}:${publicationIds.hid}/accept/`, null)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        angular.isFunction(successCallback) && successCallback(response.data)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.ACCEPT_MODERATING.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+        public accept(publicationIds: any): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.post(`/ajax/api/moderators/publications/${publicationIds.tid}:${publicationIds.hid}/accept/`, null);
+            promise.success(response => {});
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.ACCEPT_MODERATING.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+            return promise;
         }
 
 
 
-        public reject(publicationIds: any, reject_reason: string, successCallback?, errorCallback?) {
-            this.$http.post(`/ajax/api/moderators/publications/${publicationIds.tid}:${publicationIds.hid}/reject/`, {
+        public reject(publicationIds: any, reject_reason: string): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.post(`/ajax/api/moderators/publications/${publicationIds.tid}:${publicationIds.hid}/reject/`, {
                 message: reject_reason
-            })
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        angular.isFunction(successCallback) && successCallback(response.data)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.REJECT_MODERATING.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+            });
+            promise.success(response => {});
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.REJECT_MODERATING.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+            return promise;
         }
 
 
 
-        public hold(publicationIds: any, successCallback?, errorCallback?) {
-            this.$http.post(`/ajax/api/moderators/publications/${publicationIds.tid}:${publicationIds.hid}/hold/`, null)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        angular.isFunction(successCallback) && successCallback(response.data)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.HOLD_MODERATING.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+        public hold(publicationIds: any): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.post(`/ajax/api/moderators/publications/${publicationIds.tid}:${publicationIds.hid}/hold/`, null);
+            promise.success(response => {});
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.HOLD_MODERATING.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+            return promise;
         }
 
 
 
-        public sendNotice(claimId: any, noticeMessage: any, successCallback?, errorCallback?) {
-            this.$http.post(`/ajax/api/moderators/claims/${claimId}/notice/`, {
+        public sendNotice(claimId: any, noticeMessage: any): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.post(`/ajax/api/moderators/claims/${claimId}/notice/`, {
                 notice: noticeMessage
-            })
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        angular.isFunction(successCallback) && successCallback(response.data)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    //this.$mdToast.show(
-                    //    this.$mdToast.simple()
-                    //        .content(this.TXT.TOASTS.PUBLICATION.PUBLISH.TITLE)
-                    //        .position(this.toastOptions.position)
-                    //        .hideDelay(this.toastOptions.delay)
-                    //);
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+            });
+            promise.success(response => {});
+            promise.error(response => {});
+            return promise;
         }
 
 
 
-        public closeClaim(claimId: any, successCallback?, errorCallback?) {
-            this.$http.post(`/ajax/api/moderators/claims/${claimId}/close/`, null)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        angular.isFunction(successCallback) && successCallback(response.data)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    //this.$mdToast.show(
-                    //    this.$mdToast.simple()
-                    //        .content(this.TXT.TOASTS.PUBLICATION.PUBLISH.TITLE)
-                    //        .position(this.toastOptions.position)
-                    //        .hideDelay(this.toastOptions.delay)
-                    //);
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+        public closeClaim(claimId: any): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.post(`/ajax/api/moderators/claims/${claimId}/close/`, null);
+            promise.success(response => {});
+            promise.error(response => {});
+            return promise;
 
         }
     }
