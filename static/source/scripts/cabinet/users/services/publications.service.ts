21b17a61de308287d60b133d5a2cba1b527b66b2
natakopulenko <nata.kopulenko@mail.ru>
2015-08-19 13:27:15 +0300

    fixed

--- static/source/scripts/cabinet/users/services/publications.service.ts
+++ static/source/scripts/cabinet/users/services/publications.service.ts
@@ -2,7 +2,7 @@
 
 
 namespace Mappino.Cabinet.Users {
-    export class PublicationsService implements IPublicationsService {
+    export class PublicationsService {
         private briefs: IBrief[];
         private publication: IPublication;
 
@@ -30,238 +30,214 @@ namespace Mappino.Cabinet.Users {
 
 
 
-        public create(publication: IPublicationNew, successCallback?, errorCallback?) {
-            this.$http.post(`/ajax/api/cabinet/publications/`, publication)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        angular.isFunction(successCallback) && successCallback(response.data)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.CREATE.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+        public create(publication: IPublicationNew): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.post(`/ajax/api/cabinet/publications/`, publication);
+
+            promise.success(response => {});
+
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.CREATE.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+
+            return promise;
         }
 
 
 
-        public remove(publicationIds: IPublicationIds, successCallback?, errorCallback?) {
-            this.$http.delete(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/`)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        this.$state.go('publications');
-                        angular.isFunction(successCallback) && successCallback(response.data)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.REMOVE.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+        public remove(publicationIds: IPublicationIds): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.delete(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/`);
+
+            promise.success(response => {});
+
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.REMOVE.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+
+            return promise;
+
         }
 
 
 
-        public publish(publicationIds: IPublicationIds, successCallback?, errorCallback?) {
-            this.$http.put(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/publish/`, null)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        this.$state.go('publications');
-                        angular.isFunction(successCallback) && successCallback(response.data)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.PUBLISH.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+        public publish(publicationIds: IPublicationIds): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.put(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/publish/`, null);
+            promise.success(response => {});
+
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.PUBLISH.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+
+            return promise;
         }
 
 
 
-        public unpublish(publicationIds: IPublicationIds, successCallback?, errorCallback?) {
-            this.$http.put(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/unpublish/`, null)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        this.$state.go('publications');
-                        angular.isFunction(successCallback) && successCallback(response.data)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.UNPUBLISH.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+        public unpublish(publicationIds: IPublicationIds): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.put(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/unpublish/`, null);
+            promise.success(response => {});
+
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.UNPUBLISH.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+
+            return promise;
         }
 
 
 
-        public load(publicationIds: IPublicationIds, successCallback?, errorCallback?) {
-            this.$http.get(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/`)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        this.publication = response.data['data'];
-                        angular.isFunction(successCallback) && successCallback(this.publication)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.LOAD.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+        public load(publicationIds: IPublicationIds): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.get(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/`);
+            promise.success(response => {
+                this.publication = response.data;
+            });
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.LOAD.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+            return promise;
         }
 
 
 
-        public uploadPhoto(publicationIds: IPublicationIds, photo: File, successCallback?, errorCallback?) {
-            this.Upload.upload({
+        public uploadPhoto(publicationIds: IPublicationIds, photo: File): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.Upload.upload({
                 url: `/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/photos/`,
                 file: photo
-            }).then(response => {
-                if (response.data['code'] === 0) {
-                    if (!this.publication.photos) {
-                        this.publication.photos = [];
-                    }
-                    this.publication.photos.push(response.data['data']);
-                    angular.isFunction(successCallback) && successCallback(this.publication)
-                } else {
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
+            });
+            promise.success(response => {
+                if (!this.publication.photos) {
+                    this.publication.photos = [];
                 }
-            }, response => {
+                this.publication.photos.push(response.data);
+            });
+            promise.error(response => {
                 this.$mdToast.show(
                     this.$mdToast.simple()
                         .content(this.TXT.TOASTS.PUBLICATION.UPLOAD_PHOTO.TITLE)
                         .position(this.toastOptions.position)
                         .hideDelay(this.toastOptions.delay)
                 );
-                angular.isFunction(errorCallback) && errorCallback(response.data)
             });
+            return promise;
         }
 
 
 
-        public removePhoto(publicationIds: IPublicationIds, photoId, successCallback?, errorCallback?) {
-            this.$http.delete(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/photos/${photoId}/`)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        angular.forEach(this.publication.photos, (photo, index) => {
-                            if (photo && photo.hash_id == photoId) {
-                                this.publication.photos.splice(index, 1);
-                            }
-                        });
+        public removePhoto(publicationIds: IPublicationIds, photoId): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.delete(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/photos/${photoId}/`);
 
-                        angular.forEach(this.publication.photos, (photo, index) => {
-                            if (photo && photo.hash_id == response.data['data'].hash_id) {
-                                this.publication.photos[index].is_title = true;
-                            }
-                        });
+            promise.success(response => {
+                var photos = this.publication.photos;
 
-                        angular.isFunction(successCallback) && successCallback(this.publication)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
+                for (let i = 0, len = photos.length; i < len; i++) {
+                    var photo = photos[i];
+
+                    if (photo && photo.hash_id == photoId) {
+                        this.publication.photos.splice(i, 1);
                     }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.REMOVE_PHOTO.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                })
+                }
+
+                for (let i = 0, len = photos.length; i < len; i++) {
+                    var photo = photos[i];
+                    if (photo && photo.hash_id == response.data.hash_id) {
+                        photo.is_title = true;
+                    }
+                }
+
+            });
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.REMOVE_PHOTO.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+            return promise;
         }
 
 
 
-        public setTitlePhoto(publicationIds: IPublicationIds, photoId, successCallback?, errorCallback?) {
-            this.$http.put(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/photos/${photoId}/title/`, null)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        angular.forEach(this.publication.photos, (photo, index) => {
-                            photo.hash_id === photoId ? this.publication.photos[index].is_title = true :
-                                this.publication.photos[index].is_title = false;
-                        });
-
-                        angular.isFunction(successCallback) && successCallback(this.publication)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.SET_TITLE_PHOTO.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                })
+        public setTitlePhoto(publicationIds: IPublicationIds, photoId): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.put(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/photos/${photoId}/title/`, null);
+            promise.success(response => {
+                var photos = this.publication.photos;
+                for (let i = 0, len = photos.length; i < len; i++) {
+                    var photo = photos[i];
+                    photo.hash_id === photoId ? photo.is_title = true : photo.is_title = false;
+                }
+            });
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.SET_TITLE_PHOTO.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+            return promise;
         }
 
 
 
-        public checkField(publicationIds: IPublicationIds, field: IPublicationCheckField, successCallback?, errorCallback?) {
-            this.$http.put(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/`, field)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        angular.isFunction(successCallback) &&
-                        successCallback(response.data['data'] && response.data['data'].value ? response.data['data'].value : field.fieldValue);
-                    }
-                }, response => {
-                    this.$mdToast.show(this.$mdToast.simple()
+        public checkField(publicationIds: IPublicationIds, field: IPublicationCheckField): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.put(`/ajax/api/cabinet/publications/${publicationIds.tid}:${publicationIds.hid}/`, field);
+
+            promise.success(response => {});
+
+            promise.error(response => {
+                this.$mdToast.show(this.$mdToast.simple()
                         .content(this.TXT.TOASTS.PUBLICATION.CHECK_FIELD.TITLE)
                         .position(this.toastOptions.position)
                         .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+                );
+            });
+
+            return promise;
         }
 
 
+        public loadBriefs(): angular.IHttpPromise<any> {
+            var promise: angular.IHttpPromise<any> = this.$http.get(`/ajax/api/cabinet/publications/briefs/all/`);
 
-        public loadBriefs(successCallback?, errorCallback?) {
-            this.$http.get(`/ajax/api/cabinet/publications/briefs/all/`)
-                .then(response => {
-                    if (response.data['code'] === 0) {
-                        this.briefs = response.data['data'];
-                        angular.isFunction(successCallback) && successCallback(this.briefs)
-                    } else {
-                        angular.isFunction(errorCallback) && errorCallback(response.data)
-                    }
-                }, response => {
-                    this.$mdToast.show(
-                        this.$mdToast.simple()
-                            .content(this.TXT.TOASTS.PUBLICATION.LOAD_BRIEFS.TITLE)
-                            .position(this.toastOptions.position)
-                            .hideDelay(this.toastOptions.delay)
-                    );
-                    angular.isFunction(errorCallback) && errorCallback(response.data)
-                });
+            promise.success(response => {
+                this.briefs = response.data;
+            });
+
+            promise.error(response => {
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .content(this.TXT.TOASTS.PUBLICATION.LOAD_BRIEFS.TITLE)
+                        .position(this.toastOptions.position)
+                        .hideDelay(this.toastOptions.delay)
+                );
+            });
+
+            return promise;
         }
     }
 }
