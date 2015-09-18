ca58d534c818db8948723a1838d712f359409a5d
hsc <dima.chizhevsky@gmail.com>
2015-09-18 09:25:15 +0300

    markers indexes app config

--- core/markers_index/__init__.py
+++ core/markers_index/__init__.py
@@ -1,37 +1,53 @@
 # coding=utf-8
-from django.dispatch import receiver
+from django.apps.config import AppConfig
 
 from core.markers_index.models import SegmentsIndex
 from core.publications import signals
 
 
-@receiver(signals.before_publish)
-def add_publication_marker(sender, **kwargs):
-    tid = kwargs['tid']
-    hid = kwargs['hid']
-    for_sale = kwargs.get('for_sale', False)
-    for_rent = kwargs.get('for_rent', False)
-
-    # Таким чином, навіть якщо оголошення одночасно подається
-    # і на продаж і в оренду - воно попаде в 2 індекса одночасно.
-    if for_sale:
-        SegmentsIndex.add_record(tid, hid, True, False)
-    if for_rent:
-        SegmentsIndex.add_record(tid, hid, False, True)
+class MarkersIndexConfig(AppConfig):
+    name = 'core.markers_index'
 
 
-@receiver(signals.before_unpublish)
-@receiver(signals.moved_to_trash)
-@receiver(signals.deleted_permanent)
-def remove_publication_marker(sender, **kwargs):
-    tid = kwargs['tid']
-    hid = kwargs['hid']
-    for_sale = kwargs.get('for_sale', False)
-    for_rent = kwargs.get('for_rent', False)
+    def ready(self):
+        SignalsInitializer.init()
 
-    # Таким чином, навіть якщо оголошення одночасно подавалось
-    # і на продаж і в оренду - воно зникне з 2х індексів одночасно.
-    if for_sale:
-        SegmentsIndex.remove_record(tid, hid, True, False)
-    if for_rent:
-        SegmentsIndex.remove_record(tid, hid, False, True)
+
+class SignalsInitializer(object):
+    @classmethod
+    def init(cls):
+        signals.before_publish.connect(cls.__add_publication_marker)
+
+        signals.before_unpublish.connect(cls.__remove_publication_marker)
+        signals.moved_to_trash.connect(cls.__remove_publication_marker)
+        signals.deleted_permanent.connect(cls.__remove_publication_marker)
+
+
+    @staticmethod
+    def __add_publication_marker(sender, **kwargs):
+        tid = kwargs['tid']
+        hid = kwargs['hid']
+        for_sale = kwargs.get('for_sale', False)
+        for_rent = kwargs.get('for_rent', False)
+
+        # Таким чином, навіть якщо оголошення одночасно подається
+        # і на продаж і в оренду - воно попаде в 2 індекса одночасно.
+        if for_sale:
+            SegmentsIndex.add_record(tid, hid, True, False)
+        if for_rent:
+            SegmentsIndex.add_record(tid, hid, False, True)
+
+
+    @staticmethod
+    def __remove_publication_marker(sender, **kwargs):
+        tid = kwargs['tid']
+        hid = kwargs['hid']
+        for_sale = kwargs.get('for_sale', False)
+        for_rent = kwargs.get('for_rent', False)
+
+        # Таким чином, навіть якщо оголошення одночасно подавалось
+        # і на продаж і в оренду - воно зникне з 2х індексів одночасно.
+        if for_sale:
+            SegmentsIndex.remove_record(tid, hid, True, False)
+        if for_rent:
+            SegmentsIndex.remove_record(tid, hid, False, True)
