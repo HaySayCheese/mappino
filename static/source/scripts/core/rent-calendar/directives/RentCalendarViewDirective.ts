1102cca28bb4ae02b7bb0e1e96026240801f12b0
natakopulenko <nata.kopulenko@mail.ru>
2015-09-18 13:40:43 +0300

    fixed

--- static/source/scripts/core/rent-calendar/directives/RentCalendarViewDirective.ts
+++ static/source/scripts/core/rent-calendar/directives/RentCalendarViewDirective.ts
@@ -1,7 +1,13 @@
 namespace Mappino.Core.RentCalendar {
-    export function RentCalendarViewDirective(): angular.IDirective {
+    export function RentCalendarViewDirective():angular.IDirective {
         return {
+            restrict: 'E',
+            templateUrl: '/ajax/template/core/rent-calendar/view/',
 
-        }
+            link: function (scope, element, attrs, modelCtrl) {
+
+            }
+        };
     }
+    RentCalendarViewDirective.$inject = [];
 }
