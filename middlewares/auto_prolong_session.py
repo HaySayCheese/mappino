4ea4c279d2ac462fdd5bb4af4e8cef5f2644773a
hsc <dima.chizhevsky@gmail.com>
2015-09-21 10:09:47 +0300

    fix: code style

--- middlewares/auto_prolong_session.py
+++ middlewares/auto_prolong_session.py
@@ -1,13 +1,14 @@
+#coding=utf-8
 
 
 class AutoProlongSession(object):
-	"""
-	Automatically prolongs sessions for authenticated user if the request was processed fine.
-	"""
+    """
+    Automatically prolongs sessions for authenticated user if the request was processed fine.
+    """
 
-	session_expire = 60*60*24*12 # 12 days
+    session_expire = 60*60*24*12 # 12 days
 
-	def process_response(self, request, response):
-		if response.status_code == 200 and request.user.is_authenticated():
-			request.session.set_expiry(self.session_expire)
-		return response
+    def process_response(self, request, response):
+        if response.status_code == 200 and request.user.is_authenticated():
+            request.session.set_expiry(self.session_expire)
+        return response
