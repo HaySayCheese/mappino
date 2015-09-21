d7677a1653f178502b121cd68ea5041ad3d47afe
hsc <dima.chizhevsky@gmail.com>
2015-09-22 02:32:34 +0300

    fix: slots initialisation in markers index app

--- core/markers_index/__init__.py
+++ core/markers_index/__init__.py
@@ -1,53 +1,14 @@
 # coding=utf-8
 from django.apps.config import AppConfig
 
-from core.markers_index.models import SegmentsIndex
-from core.publications import signals
+from core.markers_index.slots import SlotsInitializer
 
 
-class MarkersIndexConfig(AppConfig):
+class MarkersIndex(AppConfig):
     name = 'core.markers_index'
 
 
     def ready(self):
-        SignalsInitializer.init()
-
-
-class SignalsInitializer(object):
-    @classmethod
-    def init(cls):
-        signals.before_publish.connect(cls.__add_publication_marker)
-
-        signals.before_unpublish.connect(cls.__remove_publication_marker)
-        signals.moved_to_trash.connect(cls.__remove_publication_marker)
-        signals.deleted_permanent.connect(cls.__remove_publication_marker)
-
-
-    @staticmethod
-    def __add_publication_marker(sender, **kwargs):
-        tid = kwargs['tid']
-        hid = kwargs['hid']
-        for_sale = kwargs.get('for_sale', False)
-        for_rent = kwargs.get('for_rent', False)
-
-        # Таким чином, навіть якщо оголошення одночасно подається
-        # і на продаж і в оренду - воно попаде в 2 індекса одночасно.
-        if for_sale:
-            SegmentsIndex.add_record(tid, hid, True, False)
-        if for_rent:
-            SegmentsIndex.add_record(tid, hid, False, True)
-
-
-    @staticmethod
-    def __remove_publication_marker(sender, **kwargs):
-        tid = kwargs['tid']
-        hid = kwargs['hid']
-        for_sale = kwargs.get('for_sale', False)
-        for_rent = kwargs.get('for_rent', False)
-
-        # Таким чином, навіть якщо оголошення одночасно подавалось
-        # і на продаж і в оренду - воно зникне з 2х індексів одночасно.
-        if for_sale:
-            SegmentsIndex.remove_record(tid, hid, True, False)
-        if for_rent:
-            SegmentsIndex.remove_record(tid, hid, False, True)
+        segments_index = self.get_model('SegmentsIndex')
+        self.slots_initializer = SlotsInitializer(segments_index)
+        self.slots_initializer.connect_all()
---
+++ core/markers_index/slots.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+from core.publications import signals as publication
+
+
+class SlotsInitializer(object):
+    def __init__(self, segments_index_model):
+        assert segments_index_model is not None
+
+        self.segments_index_model = segments_index_model
+
+
+    def connect_all(self):
+        # slots that handle publication publication and creation
+        publication.before_publish.connect(self.__add_publication_marker)
+
+        # slots that handles publication removing and unpublishing
+        publication.before_unpublish.connect(self.__remove_publication_marker)
+        publication.moved_to_trash.connect(self.__remove_publication_marker)
+        publication.deleted_permanent.connect(self.__remove_publication_marker)
+
+
+    def __add_publication_marker(self, sender, **kwargs):
+        tid = kwargs['tid']
+        hid = kwargs['hid']
+        for_sale = kwargs.get('for_sale', False)
+        for_rent = kwargs.get('for_rent', False)
+
+        # Таким чином, навіть якщо оголошення одночасно подається
+        # і на продаж і в оренду - воно попаде в 2 індекса одночасно.
+        if for_sale:
+            self.segments_index_model.add_record(tid, hid, True, False)
+        if for_rent:
+            self.segments_index_model.add_record(tid, hid, False, True)
+
+
+    def __remove_publication_marker(self, sender, **kwargs):
+        tid = kwargs['tid']
+        hid = kwargs['hid']
+        for_sale = kwargs.get('for_sale', False)
+        for_rent = kwargs.get('for_rent', False)
+
+        # Таким чином, навіть якщо оголошення одночасно подавалось
+        # і на продаж і в оренду - воно зникне з 2х індексів одночасно.
+        if for_sale:
+            self.segments_index_model.remove_record(tid, hid, True, False)
+        if for_rent:
+            self.segments_index_model.remove_record(tid, hid, False, True)
