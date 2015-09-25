38a546ebb84d56d7dc125a435fc5bbb71c4cf604
serhii <serhii.meketiuk@gmail.com>
2015-09-25 17:13:18 +0300

    fixed

--- static/source/scripts/cabinet/users/configs/MaterialConfigs.ts
+++ static/source/scripts/cabinet/users/configs/MaterialConfigs.ts
@@ -7,10 +7,13 @@ namespace Mappino.Cabinet.Users {
     export class MaterialConfigs {
 
         constructor(private app: ng.IModule) {
-            app.config(['$mdThemingProvider', ($mdThemingProvider) => {
+            app.config(['$mdThemingProvider', '$mdDateLocaleProvider', ($mdThemingProvider, $mdDateLocaleProvider) => {
                 $mdThemingProvider.theme('default')
                     .primaryPalette('blue')
                     .accentPalette('deep-orange');
+
+                $mdDateLocaleProvider.days = ['Понедылок', 'Вывторок', 'Середа', 'Четвер', 'Пятниця', 'Субота', 'Неділя'];
+                $mdDateLocaleProvider.shortDays = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Нд'];
             }]);
         }
     }
