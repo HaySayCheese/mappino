6bcf113a3757109db0dba5902cbb4acbefecbd18
serhii <serhii.meketiuk@gmail.com>
2015-09-28 16:44:04 +0300

    fixed

--- static/source/scripts/cabinet/users/controllers/PublishedPublicationController.ts
+++ static/source/scripts/cabinet/users/controllers/PublishedPublicationController.ts
@@ -10,11 +10,20 @@ namespace Mappino.Cabinet.Users  {
 
         public static $inject = [
             '$scope',
-            '$state'
+            '$state',
+            '$rootScope',
+            '$mdDialog',
+            'TXT',
+            'PublicationsService'
         ];
 
         constructor(private $scope,
-                    private $state) {
+                    private $state,
+                    private $rootScope: any,
+                    private $mdDialog: any,
+                    private TXT: any,
+                    private publicationsService: PublicationsService) {
+            // ---------------------------------------------------------------------------------------------------------
             $scope.showRentDetails = false;
 
             if ($state.params['publication_id'] && $state.params['publication_id'] != 0) {
@@ -31,5 +40,59 @@ namespace Mappino.Cabinet.Users  {
 
         }
 
+
+
+        public removePublication($event) {
+            var confirm = this.$mdDialog.confirm()
+                .parent(angular.element(document.body))
+                .title(this.TXT.DIALOGS.REMOVE_PUBLICATION.TITLE)
+                .content(this.TXT.DIALOGS.REMOVE_PUBLICATION.BODY)
+                .ariaLabel(this.TXT.DIALOGS.REMOVE_PUBLICATION.ARIA_LABEL)
+                .ok(this.TXT.DIALOGS.REMOVE_PUBLICATION.OK_BTN)
+                .cancel(this.TXT.DIALOGS.REMOVE_PUBLICATION.CANCEL_BTN)
+                .targetEvent($event);
+
+
+            this.$mdDialog.show(confirm).then(() => {
+                this.$rootScope.loaders.overlay = true;
+
+                this.publicationsService.remove({ tid: this.publicationIds.tid, hid: this.publicationIds.hid })
+                    .success(response => {
+                        this.$state.go('publications');
+
+                        this.$rootScope.loaders.overlay = false;
+                    })
+                    .error(response => {
+                        this.$rootScope.loaders.overlay = false;
+                    });
+            });
+        }
+
+
+
+        public editPublication($event) {
+            var confirm = this.$mdDialog.confirm()
+                .parent(angular.element(document.body))
+                .title(this.TXT.DIALOGS.EDIT_PUBLICATION.TITLE)
+                .content(this.TXT.DIALOGS.EDIT_PUBLICATION.BODY)
+                .ariaLabel(this.TXT.DIALOGS.EDIT_PUBLICATION.ARIA_LABEL)
+                .ok(this.TXT.DIALOGS.EDIT_PUBLICATION.OK_BTN)
+                .cancel(this.TXT.DIALOGS.EDIT_PUBLICATION.CANCEL_BTN)
+                .targetEvent($event);
+
+
+            this.$mdDialog.show(confirm).then(() => {
+                this.$rootScope.loaders.overlay = true;
+
+                this.publicationsService.unpublish({ tid: this.publicationIds.tid, hid: this.publicationIds.hid })
+                    .success(response => {
+                    this.$rootScope.loaders.overlay = false;
+                    this.$state.go('publication_edit', { publication_id: this.publicationIds.tid + ':' + this.publicationIds.hid });
+                })
+                    .error(response => {
+                    this.$rootScope.loaders.overlay = false;
+                });
+            });
+        }
     }
 }
