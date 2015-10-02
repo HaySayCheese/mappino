91cb76296b1596433f40d1ba423e1e4b5895bb6f
natakopulenko <nata.kopulenko@mail.ru>
2015-10-02 23:21:16 +0300

    fixed

--- static/source/scripts/landing/controllers/LandingController.ts
+++ static/source/scripts/landing/controllers/LandingController.ts
@@ -6,12 +6,16 @@ namespace Mappino.Landing {
         public static $inject = [
             '$scope',
             '$mdMedia',
-            '$mdSidenav'
+            '$mdSidenav',
+            '$location',
+            '$rootScope'
         ];
 
         constructor(private $scope: any,
                     private $mdMedia: any,
-                    private $mdSidenav: any) {
+                    private $mdSidenav: any,
+                    private $location: ng.ILocationService,
+                    private $rootScope: any) {
             // ---------------------------------------------------------------------------------------------------------
             $scope.search = {
                 realty_type_sid: 0,
@@ -19,11 +23,25 @@ namespace Mappino.Landing {
                 period_type_sid: 0,
                 date_enter: undefined,
                 date_leave: undefined,
-                city: ''
+                city: '',
+                l: ''
             };
-
             $scope.$watch(() => $mdMedia('sm'), (isSmall) => !isSmall && this.$mdSidenav('left-sidenav').close());
+            this.$scope.search.l = "48.455935,34.41285";
+            $scope.$watch('search.operation_sid', newValue => {
+                if (newValue == 2) {
+                    this.$scope.operation_sid = 1;
+                    this.$scope.url_part1 = '&b_r_d_min=';
+                    this.$scope.url_part2 ='&b_r_d_max=';
+                    this.$scope.url_part3 ='&b_pr_sid=0';
+                }
+                else {
+                    this.$scope.operation_sid = this.$scope.search.operation_sid;
+                    this.$scope.url_part1 = '';
+                    this.$scope.url_part2 = '';
 
+                }
+            });
             this.initAutocomplete(document.getElementById('landing-autucomplete'));
         }
 
@@ -46,8 +64,14 @@ namespace Mappino.Landing {
             });
 
             google.maps.event.addListener(autocomplete, 'place_changed', () => {
-                this.$scope.search.city = autocomplete.getPlace().formatted_address
+                var place: any;
+                place = autocomplete.getPlace();
+                this.$scope.search.l = autocomplete.getPlace().geometry.location;
+                this.$scope.search.city = place.formatted_address;
+                this.$scope.search.l = place.geometry.location.lat().toString() + ',' + place.geometry.location.lng().toString();
+
             });
         }
+
     }
 }
