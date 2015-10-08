2143a6fb5c63a13c9a32734405423bf3f3e2b13a
Dima Chizhevsky <HaySayCheese@gmail.com>
2015-10-08 10:46:31 +0300

    fix

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,13 +1,15 @@
 # coding=utf-8
-from django.http.response import HttpResponseRedirect
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import etag
 from collective.decorators.jinja2_shortcuts import render_jinja2_template
 from core.cache.utils import generate_template_etag
 
 
+@etag(generate_template_etag('help/help.html'))
+@ensure_csrf_cookie
+@render_jinja2_template
 def help(request):
-    return HttpResponseRedirect('help/about/')
+    return 'help/help.html'
 
 
 @etag(generate_template_etag('landing/landing.html'))
